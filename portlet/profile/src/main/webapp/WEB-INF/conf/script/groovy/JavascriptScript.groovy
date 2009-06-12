c619e5ede02ae05e538c18af7bdc3bf2d1fb6f06
Nguyen Dang Tung <tung.dang@exoplatform.com>
2009-06-12 03:25:57 +0000

    create javascript template for timezone of gadget, create new template for activities portlet (gadget - canvas mode)

---
+++ portlet/profile/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
@@ -0,0 +1,2 @@
+/*Add JavaScript(s) that reused in many page to here. They will be merged in one file dynamically at server launch time.*/
+JavascriptService.addJavascript("eXo.social.activities.UIActivities", "/javascript/eXo/social/profile/UIActivities.js", ServletContext);
