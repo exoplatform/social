b9ff301ba74a92892c8284c0fad033f634a461fb
Hanh Vi <hanh.vi@exoplatform.com>
2009-08-20 06:53:13 +0000

    SOC-215 Rewrite some form of Profile using UIForm

--- portlet/profile/src/main/java/org/exoplatform/social/portlet/profile/UIBasicInfoSection.java
+++ portlet/profile/src/main/java/org/exoplatform/social/portlet/profile/UIBasicInfoSection.java
@@ -16,11 +16,25 @@
  */
 package org.exoplatform.social.portlet.profile;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.social.core.identity.IdentityManager;
+import org.exoplatform.social.core.identity.model.Profile;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
 import org.exoplatform.webui.core.UIComponent;
 import org.exoplatform.webui.core.lifecycle.UIFormLifecycle;
+import org.exoplatform.webui.core.model.SelectItemOption;
 import org.exoplatform.webui.event.Event;
+import org.exoplatform.webui.event.Event.Phase;
+import org.exoplatform.webui.form.UIFormSelectBox;
+import org.exoplatform.webui.form.UIFormStringInput;
+import org.exoplatform.webui.form.validator.ExpressionValidator;
+import org.exoplatform.webui.form.validator.MandatoryValidator;
+import org.exoplatform.webui.form.validator.StringLengthValidator;
 
 /**
  * Created by The eXo Platform SARL
@@ -33,32 +47,95 @@ import org.exoplatform.webui.event.Event;
     lifecycle = UIFormLifecycle.class,
     template =  "app:/groovy/portal/webui/component/UIBasicInfoSection.gtmpl",
     events = {
-        @EventConfig(listeners = UIProfileSection.EditActionListener.class),
+        @EventConfig(listeners = UIProfileSection.EditActionListener.class, phase=Phase.DECODE),
         @EventConfig(listeners = UIBasicInfoSection.SaveActionListener.class),
         @EventConfig(listeners = UIProfileSection.CancelActionListener.class)
     }
 )
+
 public class UIBasicInfoSection extends UIProfileSection {
 
   public UIBasicInfoSection() throws Exception {
     addChild(UITitleBar.class, null, null);
-  }
+    
+    addUIFormInput(new UIFormStringInput("firstName", "firstname", null)
+                   .addValidator(MandatoryValidator.class)
+                   .addValidator(StringLengthValidator.class, 3, 30)
+                   .addValidator(ExpressionValidator.class, "^\\p{L}[\\p{L}\\d._,\\s]+\\p{L}$", "UIBasicInfoSection.msg.Invalid-char"));
+    addUIFormInput(new UIFormStringInput("lastName", "lastname", null)
+                   .addValidator(MandatoryValidator.class)
+                   .addValidator(StringLengthValidator.class, 3, 30)
+                   .addValidator(ExpressionValidator.class, "^\\p{L}[\\p{L}\\d._,\\s]+\\p{L}$", "UIBasicInfoSection.msg.Invalid-char"));
 
+    List<SelectItemOption<String>> options = new ArrayList<SelectItemOption<String>>();
+    options.add(new SelectItemOption<String>("male")) ;
+    options.add(new SelectItemOption<String>("female")) ;
+    addUIFormInput(new UIFormSelectBox("gender", "gender", options));
+  }
+  
+  /**
+   * Store profile information into database when form is submitted.
+   *
+   */
   public static class SaveActionListener extends UIProfileSection.SaveActionListener {
 
     public void execute(Event<UIProfileSection> event) throws Exception {
-      System.out.println("execute SaveActionListener of UIBasicInfoSection");
       super.execute(event);
-      UIProfileSection sect = event.getSource();
-              
-      //we try to get the UIHeaderSection to refresh it since it contains also
-      //the firstname and lastname
+      
+      UIProfileSection sect = event.getSource();    
+      
+      saveProfile((UIBasicInfoSection)sect);      
+      
+      //Get the UIHeaderSection to refresh it since it contains also the firstname and lastname
       UIComponent parent = sect.getParent();
       UIProfileSection header = parent.findFirstComponentOfType(UIHeaderSection.class);
       if(header != null) {
         event.getRequestContext().addUIComponentToUpdateByAjax(header);
       }
-    }
+    }       
+  }  
+  
+  /**
+   * Get information from profile and set value into uicomponent.
+   * 
+   * @throws Exception
+   */
+  public void setValue() throws Exception {
+    Profile profile = getProfile();
+    String firstName = (String) profile.getProperty("firstName");
+    firstName = (firstName == null ? "": firstName);
+    String lastName = (String) profile.getProperty("lastName");
+    lastName = (lastName == null ? "": lastName);
+    String gender = (String) profile.getProperty("gender");
+    UIFormStringInput uiFirstName = getChildById("firstName");
+    UIFormStringInput uiLastName = getChildById("lastName");
+    UIFormSelectBox uiGender = getChildById("gender");
+    uiFirstName.setValue(firstName);
+    uiLastName.setValue(lastName);
+    uiGender.setValue(gender);    
   }
-
+  
+  /**
+   * Store profile information into database.
+   * 
+   * @param uiBasicInfoSection
+   * @throws Exception
+   */
+  private static void saveProfile(UIBasicInfoSection uiBasicInfoSection) throws Exception {
+    UIFormStringInput uiFirstName = uiBasicInfoSection.getChildById("firstName");
+    UIFormStringInput uiLastName = uiBasicInfoSection.getChildById("lastName");
+    UIFormSelectBox uiGender = uiBasicInfoSection.getChildById("gender");
+    String firstName = uiFirstName.getValue();
+    String lastName = uiLastName.getValue();
+    String gender = uiGender.getValue();
+    
+    ExoContainer container = ExoContainerContext.getCurrentContainer();
+    IdentityManager im = (IdentityManager) container.getComponentInstanceOfType(IdentityManager.class);
+    Profile p = uiBasicInfoSection.getProfile();
+    p.setProperty("firstName", firstName);
+    p.setProperty("lastName", lastName);
+    p.setProperty("gender", gender);
+    
+    im.saveProfile(p);    
+  }
 }
