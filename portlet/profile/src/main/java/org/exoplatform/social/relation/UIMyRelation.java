c765e2cdec7d1638922b8b1b920f68db55affda1
Nguyen Dang Tung <tung.dang@exoplatform.com>
2009-08-26 10:54:37 +0000

    SOC-227 creat new vertial porltet for new template relation

--- portlet/profile/src/main/java/org/exoplatform/social/relation/UIMyRelation.java
+++ portlet/profile/src/main/java/org/exoplatform/social/relation/UIMyRelation.java
@@ -16,7 +16,22 @@
  */
 package org.exoplatform.social.relation;
 
+import java.util.List;
+
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.portal.application.PortalRequestContext;
+import org.exoplatform.portal.webui.util.Util;
+import org.exoplatform.social.core.identity.IdentityManager;
+import org.exoplatform.social.core.identity.impl.organization.OrganizationIdentityProvider;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.relationship.Relationship;
+import org.exoplatform.social.core.relationship.RelationshipManager;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.config.annotation.EventConfig;
+import org.exoplatform.webui.core.lifecycle.UIFormLifecycle;
+import org.exoplatform.webui.event.Event;
+import org.exoplatform.webui.event.EventListener;
 import org.exoplatform.webui.form.UIForm;
 
 /**
@@ -26,7 +41,78 @@ import org.exoplatform.webui.form.UIForm;
  * Aug 25, 2009  
  */
 @ComponentConfig(
-    template =  "app:/groovy/portal/webui/component/UIMyRelation.gtmpl"
+    lifecycle = UIFormLifecycle.class,
+    template =  "app:/groovy/portal/webui/component/UIMyRelation.gtmpl",
+    events = {
+        @EventConfig(listeners = UIMyRelation.RemoveActionListener.class) 
+      }
 )
 public class UIMyRelation extends UIForm {
+  
+  private RelationshipManager relationshipManager;
+  private Identity currIdentity = null;
+  private IdentityManager identityManager = null;
+  
+  public List<Relationship> getMyRelation() throws Exception {
+    RelationshipManager relationshipManager = getRelationshipManager();
+    Identity currId = getCurrentIdentity();
+    return relationshipManager.getContacts(currId);
+  }
+  
+  public List<Relationship> getInvitedRelation() throws Exception {
+    RelationshipManager relationshipManager = getRelationshipManager();
+    Identity currId = getCurrentIdentity();
+    return relationshipManager.getContacts(currId);
+  }
+  
+  private RelationshipManager getRelationshipManager() {
+    if(relationshipManager == null) {
+      ExoContainer container = ExoContainerContext.getCurrentContainer();
+      relationshipManager = (RelationshipManager) container.getComponentInstanceOfType(RelationshipManager.class);
+    }
+    return relationshipManager;
+  }
+  
+  public Identity getCurrentIdentity() throws Exception {
+    if (currIdentity == null) {
+      IdentityManager im = getIdentityManager();
+      currIdentity = im.getIdentityByRemoteId("organization", getCurrentUserName());
+    }
+    return currIdentity;
+  }
+  
+  static public class RemoveActionListener extends EventListener<UIMyRelation> {
+    @Override
+    public void execute(Event<UIMyRelation> event) throws Exception {
+      UIMyRelation uiMyRelation = event.getSource();
+      String identityId = event.getRequestContext().getRequestParameter(OBJECTID);
+      String currUserId = uiMyRelation.getCurrentUserName();
+
+      IdentityManager im = uiMyRelation.getIdentityManager();
+      Identity currIdentity = im.getIdentityByRemoteId(OrganizationIdentityProvider.NAME,
+                                                       currUserId);
+
+      Identity requestedIdentity = im.getIdentityById(identityId);
+
+      RelationshipManager rm = uiMyRelation.getRelationshipManager();
+
+      Relationship rel = rm.getRelationship(currIdentity, requestedIdentity);
+      if (rel != null)
+        rm.remove(rel);
+    }
+  }
+  
+  private IdentityManager getIdentityManager() {
+    if(identityManager == null) {
+      ExoContainer container = ExoContainerContext.getCurrentContainer();
+      identityManager =  (IdentityManager) container.getComponentInstanceOfType(IdentityManager.class);
+    }
+    return identityManager;
+  }
+  
+  private String getCurrentUserName() {
+    PortalRequestContext portalRequest = Util.getPortalRequestContext();
+    return portalRequest.getRemoteUser();
+  }
+ 
 }
