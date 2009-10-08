9916f63ad835410dcf2013f4215b16f411d008ea
Nguyen Dang Tung <tung.dang@exoplatform.com>
2009-10-08 03:28:02 +0000

    SOC-335 apply i18n feature for gadget

--- component/opensocial/src/main/java/org/exoplatform/social/opensocial/ExoSocialApiGuiceModule.java
+++ component/opensocial/src/main/java/org/exoplatform/social/opensocial/ExoSocialApiGuiceModule.java
@@ -16,9 +16,22 @@
  */
 package org.exoplatform.social.opensocial;
 
-import org.apache.shindig.social.core.config.SocialApiGuiceModule;
+import java.util.Set;
+
+import org.apache.shindig.common.servlet.ParameterFetcher;
+import org.apache.shindig.protocol.DataServiceServletFetcher;
+import org.apache.shindig.protocol.conversion.BeanConverter;
+import org.apache.shindig.protocol.conversion.BeanJsonConverter;
+import org.apache.shindig.protocol.conversion.BeanXStreamConverter;
+import org.apache.shindig.protocol.conversion.xstream.XStreamConfiguration;
+import org.apache.shindig.social.core.util.BeanXStreamAtomConverter;
+import org.apache.shindig.social.core.util.xstream.XStream081Configuration;
 import org.apache.shindig.social.opensocial.model.Person;
 import org.apache.shindig.social.opensocial.oauth.OAuthDataStore;
+import org.apache.shindig.social.opensocial.service.ActivityHandler;
+import org.apache.shindig.social.opensocial.service.AppDataHandler;
+import org.apache.shindig.social.opensocial.service.MessageHandler;
+import org.apache.shindig.social.opensocial.service.PersonHandler;
 import org.apache.shindig.social.opensocial.spi.ActivityService;
 import org.apache.shindig.social.opensocial.spi.AppDataService;
 import org.apache.shindig.social.opensocial.spi.PersonService;
@@ -27,21 +40,62 @@ import org.exoplatform.social.opensocial.oauth.EXoOAuthDataStore;
 import org.exoplatform.social.opensocial.spi.ExoActivityService;
 import org.exoplatform.social.opensocial.spi.ExoPeopleService;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
 import com.google.inject.name.Names;
 
-public class ExoSocialApiGuiceModule  extends SocialApiGuiceModule {
 
-  public ExoSocialApiGuiceModule() {
-    super();
-  }
+/**
+ * Created by The eXo Platform SARL
+ * Modify : dang.tung
+ *          tungcnw@gmail.com
+ * Oct 7, 2009          
+ */
+
+/*
+ * TODO tung.dang: this class stand for social Guice module of social project
+ *            because of some conflict Guice module of portal and social so I
+ *            have to extends this class to AbstractModule (should be SocialApiGuiceModule)
+ *            NEED to improve it when update portal, social and shindig. 
+ * 
+ * */
+public class ExoSocialApiGuiceModule  extends AbstractModule {
+
+  
+//  public ExoSocialApiGuiceModule() {
+//    super();
+//  }
 
   /** {@inheritDoc} */
   @Override
   protected void configure() {
-    super.configure();
+    //super.configure();
 
+    bind(ParameterFetcher.class).annotatedWith(Names.named("DataServiceServlet"))
+    .to(DataServiceServletFetcher.class);
+
+//    bind(Boolean.class)
+//        .annotatedWith(Names.named(AnonymousAuthenticationHandler.ALLOW_UNAUTHENTICATED))
+//        .toInstance(Boolean.TRUE);
+    bind(XStreamConfiguration.class).to(XStream081Configuration.class);
+    bind(BeanConverter.class).annotatedWith(Names.named("shindig.bean.converter.xml")).to(
+        BeanXStreamConverter.class);
+    bind(BeanConverter.class).annotatedWith(Names.named("shindig.bean.converter.json")).to(
+        BeanJsonConverter.class);
+    bind(BeanConverter.class).annotatedWith(Names.named("shindig.bean.converter.atom")).to(
+        BeanXStreamAtomConverter.class);
+    
+//    bind(new TypeLiteral<List<AuthenticationHandler>>(){}).toProvider(
+//        AuthenticationHandlerProvider.class);
+    
+    bind(new TypeLiteral<Set<Object>>(){}).annotatedWith(Names.named("org.apache.shindig.social.handlers"))
+        .toInstance(getHandlers());
+    
     bind(String.class).annotatedWith(Names.named("shindig.canonical.json.db"))
     .toInstance("sampledata/canonicaldb.json");
+    
+    //TODO dang.tung - for exo social
     bind(PersonService.class).to(ExoPeopleService.class);
     bind(AppDataService.class).to(ExoPeopleService.class);
     bind(ActivityService.class).to(ExoActivityService.class);
@@ -51,5 +105,13 @@ public class ExoSocialApiGuiceModule  extends SocialApiGuiceModule {
     
   }
 
-
+  /**
+   * Hook to provide a Set of request handlers.  Subclasses may override
+   * to add or replace additional handlers.
+   */
+  protected Set<Object> getHandlers() {
+    return ImmutableSet.<Object>of(ActivityHandler.class, AppDataHandler.class,
+        PersonHandler.class, MessageHandler.class);
+  }
+  
 }
