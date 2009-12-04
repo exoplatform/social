94fe608bf677ca175b8f9ecc5c359425e8564f18
Hieu Trung Luong <hieult2@exoplatform.com>
2009-12-04 09:12:30 +0000

    [Social] Create button Delete for Status Update Gadget

--- web/opensocial/src/main/webapp/gadgets/activities2/script/eXo/social/StatusUpdate.js
+++ web/opensocial/src/main/webapp/gadgets/activities2/script/eXo/social/StatusUpdate.js
@@ -438,6 +438,16 @@ eXo.social.StatusUpdate.prototype.handleActivities = function(dataResponse, data
   			html.push('<div class="Content">');
   				html.push('<div class="TitleItem" href="#">' + title + '</div>');
 				html.push('<div class="ActionContenButton" href="#"><span></span></div>');
+				html.push('<div style="position: absolute; display: block; min-width: 98px; top: 30px; right: 40px; width: 129px; visibility: hidden;" class="MenuItemContainer" id="#">');
+					html.push('<div class="SubBlock">');
+						html.push('<div class="MenuItem">');
+							html.push('<a class="ItemIcon DeleteIcon GroupIcon" title="" href="">Delete</a>');
+						html.push('</div>');
+						html.push('<div class="MenuItem">');
+							html.push('<a class="ItemIcon EditIcon GroupIcon" title="" href="">Edit Page</a>');
+						html.push('</div>');
+					html.push('</div>');
+				html.push('</div>');
 				html.push('<div style="clear: both; height: 0px;"><span></span></div>');
   				html.push('<div class="Content">' + body + '</div>');
   				html.push('<div class="NewsDate">' + prettyTime + '</div>');
