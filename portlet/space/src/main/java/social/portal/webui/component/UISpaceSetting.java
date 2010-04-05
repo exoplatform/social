a2b6b4bfe3ad6b0cee14dd6ea6bc574c43a6de40
Hoat Le <hoat.le@exoplatform.com>
2010-04-05 18:02:29 +0000

    SOC-568: Bug JS: Show wrong in Application list form

--- portlet/space/src/main/java/social/portal/webui/component/UISpaceSetting.java
+++ portlet/space/src/main/java/social/portal/webui/component/UISpaceSetting.java
@@ -20,37 +20,56 @@ import org.exoplatform.portal.webui.util.Util;
 import org.exoplatform.social.space.Space;
 import org.exoplatform.social.space.SpaceException;
 import org.exoplatform.social.space.SpaceService;
+import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.config.annotation.ComponentConfigs;
+import org.exoplatform.webui.config.annotation.EventConfig;
+import org.exoplatform.webui.core.UIComponent;
 import org.exoplatform.webui.core.UIContainer;
+import org.exoplatform.webui.core.UIPopupContainer;
 import org.exoplatform.webui.core.UITabPane;
+import org.exoplatform.webui.event.Event;
+import org.exoplatform.webui.event.EventListener;
+
 /**
  * {@link UISpaceSetting} used to manage space info, permission and members. <br />
  * Created by The eXo Platform SARL
+ * 
  * @author <a href="mailto:tungcnw@gmail.com">dang.tung</a>
  * @since Sep 12, 2008
  */
-
-@ComponentConfig(
-    template =  "app:/groovy/portal/webui/uiform/UISpaceSetting.gtmpl"
-)
+@ComponentConfigs({
+  @ComponentConfig(
+    template = "app:/groovy/portal/webui/uiform/UISpaceSetting.gtmpl"
+  ),
+  @ComponentConfig(
+    type = UITabPane.class,
+    id = "UISpaceSettingTabPane",
+    template = "system:/groovy/webui/core/UITabPane_New.gtmpl",
+    events = { @EventConfig(listeners = UISpaceSetting.SelectTabActionListener.class) })
+  })
 public class UISpaceSetting extends UIContainer {
 
   private Space space;
+
   /**
    * constructor
+   * 
    * @throws Exception
    */
   public UISpaceSetting() throws Exception {
     UITabPane uiTabPane = addChild(UITabPane.class, null, null);
+    uiTabPane.setComponentConfig(UITabPane.class, "UISpaceSettingTabPane");
     uiTabPane.addChild(UISpaceInfo.class, null, null);
     uiTabPane.addChild(UISpacePermission.class, null, null);
     uiTabPane.addChild(UISpaceMember.class, null, null);
     uiTabPane.addChild(UISpaceApplication.class, null, null);
     uiTabPane.setSelectedTab(1);
   }
-  
+
   /**
    * sets space to work with
+   * 
    * @param space
    * @throws Exception
    */
@@ -65,9 +84,10 @@ public class UISpaceSetting extends UIContainer {
     uiSpacePermission.setValue(space);
     this.space = space;
   }
-  
+
   /**
    * gets space name
+   * 
    * @return space name
    */
   public String getSpaceName() {
@@ -78,20 +98,44 @@ public class UISpaceSetting extends UIContainer {
       return null;
     }
   }
-  
+
   /**
    * checks if the remote user is leader.
+   * 
    * @return true or false
    * @throws Exception
    */
-  public boolean isLeader() throws Exception{    
-    SpaceService spaceSrc = getApplicationComponent(SpaceService.class);    
-    String userId = Util.getPortalRequestContext().getRemoteUser(); 
-    if(spaceSrc.hasEditPermission(space, userId)) {                       
-      return true;      
+  public boolean isLeader() throws Exception {
+    SpaceService spaceSrc = getApplicationComponent(SpaceService.class);
+    String userId = Util.getPortalRequestContext().getRemoteUser();
+    if (spaceSrc.hasEditPermission(space, userId)) {
+      return true;
     } else {
       return false;
     }
   }
-  
+
+  /**
+   * This action listener is activated when user changes tab in space setting.
+   * When selecting tabs, deactivates application add popup from application tab
+   * @author hoatle
+   */
+  static public class SelectTabActionListener extends EventListener<UITabPane> {
+    public void execute(Event<UITabPane> event) throws Exception {
+      UITabPane uiTabPane = event.getSource();
+      WebuiRequestContext context = event.getRequestContext();
+      String renderTab = context.getRequestParameter(UIComponent.OBJECTID);
+      if (renderTab == null)
+        return;
+      if (!renderTab.equals(UISpaceApplication.class.getSimpleName())) {
+        UISpaceApplication uiApplication = uiTabPane.getChild(UISpaceApplication.class);
+        if (uiApplication != null) {
+          UIPopupContainer uiPopupContainer = uiApplication.getChild(UIPopupContainer.class);
+          uiPopupContainer.deActivate();
+        }
+      }
+      uiTabPane.setSelectedTab(renderTab);
+      context.setResponseComplete(true);
+    }
+  }
 }
