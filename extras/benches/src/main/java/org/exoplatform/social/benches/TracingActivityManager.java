f613a51388e9613a7aa1553b8ebd94b6edccf5bc
Patrice Lamarque <patrice.lamarque@exoplatform.com>
2010-04-22 08:10:42 +0000

    SOC-827 : overloaded activitymanager that logs duration of operations

---
+++ extras/benches/src/main/java/org/exoplatform/social/benches/TracingActivityManager.java
@@ -0,0 +1,42 @@
+package org.exoplatform.social.benches;
+
+import java.util.List;
+
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.social.core.activitystream.ActivityManager;
+import org.exoplatform.social.core.activitystream.model.Activity;
+import org.exoplatform.social.core.identity.IdentityManager;
+import org.exoplatform.social.core.identity.model.Identity;
+
+import org.exoplatform.social.space.impl.SocialDataLocation;
+
+/**
+ * replacement component for ActivityManager that decorates entry methods by
+ * perf logs
+ * 
+ * @author <a href="mailto:patrice.lamarque@exoplatform.com">Patrice
+ *         Lamarque</a>
+ * @version $Revision$
+ */
+public class TracingActivityManager extends ActivityManager {
+
+  private ActivityManager  activityManager;
+
+  private static final Log LOG = ExoLogger.getExoLogger(TracingActivityManager.class);
+
+  public TracingActivityManager(SocialDataLocation dataLocation,
+                                IdentityManager identityManager) throws Exception {
+    super(dataLocation, identityManager);
+    this.activityManager = new ActivityManager (dataLocation, identityManager);
+  }
+
+  public List<Activity> getActivities(Identity identity) throws Exception  {
+    long t1 = System.currentTimeMillis();
+    try {
+      return activityManager.getActivities(identity);
+       } finally {
+      LOG.info("getActivities() : " + (System.currentTimeMillis() - t1) + "ms");
+  }
+  }
+}
