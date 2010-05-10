84d2bd0a7a7e7979a0e50a845e4e61b76ad01a09
Nguyen Trong Nghia <nghia.nguyen@exoplatform.com>
2010-05-10 11:14:11 +0000

    SOC-695:Show message wrong when edit space has some special chars

--- portlet/space/src/main/java/social/portal/webui/component/UISpaceSettings.java
+++ portlet/space/src/main/java/social/portal/webui/component/UISpaceSettings.java
@@ -16,9 +16,6 @@
  */
 package social.portal.webui.component;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.exoplatform.social.space.Space;
 import org.exoplatform.webui.core.model.SelectItemOption;
 import org.exoplatform.webui.form.UIFormInputSet;
@@ -29,55 +26,61 @@ import org.exoplatform.webui.form.validator.ExpressionValidator;
 import org.exoplatform.webui.form.validator.MandatoryValidator;
 import org.exoplatform.webui.form.validator.StringLengthValidator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This UI is used in UIAddSpaceForm. <br />
  * Setting variables: <br />
- *        - Name <br />
- *        - Priority <br />
- *        - Description <br />
- *        
+ * - Name <br />
+ * - Priority <br />
+ * - Description <br />
+ * <p/>
  * Created by The eXo Platform SAS
+ *
  * @author <a href="mailto:hoatlevan@gmail.com">hoatle</a>
  * @since Jul 1, 2009
  */
 
 public class UISpaceSettings extends UIFormInputSet {
-  private final String SPACE_NAME = "name";
-  private final String SPACE_PRIORITY = "priority";
-  private final String SPACE_DESCRIPTION = "description";
-  
-  //These priority variables should be set in Space.java model
-  private final String PRIORITY_HIGH = "high";
-  private final String PRIORITY_INTERMEDIATE = "intermediate";
-  private final String PRIORITY_LOW = "low";
-  
-  //Message
-  private final String MSG_INVALID_SPACE_NAME = "UISpaceSettings.msg.invalid_space_name";
-  /**
-   * constructor
-   * @param name
-   * @throws Exception
-   */
-  public UISpaceSettings(String name) throws Exception {
-    super(name);
-    addUIFormInput(new UIFormStringInput(SPACE_NAME, SPACE_NAME, null)
-       .addValidator(MandatoryValidator.class)
-       .addValidator(StringLengthValidator.class, 3, 30)
-       .addValidator(ExpressionValidator.class, "^[\\p{L}][\\p{L}._\\- \\d]+$", "ResourceValidator.msg.Invalid-char")
-       .addValidator(ExpressionValidator.class, "^[\\p{L}][\\p{ASCII}]+$", MSG_INVALID_SPACE_NAME));
-                  
-    List<SelectItemOption<String>> priorityList = new ArrayList<SelectItemOption<String>>(3);
-    SelectItemOption<String> pHight = new SelectItemOption<String>(PRIORITY_HIGH, Space.HIGH_PRIORITY);
-    SelectItemOption<String> pImmediate = new SelectItemOption<String>(PRIORITY_INTERMEDIATE, Space.INTERMEDIATE_PRIORITY);
-    SelectItemOption<String> pLow = new SelectItemOption<String>(PRIORITY_LOW, Space.LOW_PRIORITY);
-    priorityList.add(pHight);
-    priorityList.add(pImmediate);
-    priorityList.add(pLow);
-    pImmediate.setSelected(true);
-        
-    UIFormSelectBox selectPriority = new UIFormSelectBox(SPACE_PRIORITY, SPACE_PRIORITY, priorityList);
-    addUIFormInput(selectPriority);
-    addUIFormInput(new UIFormTextAreaInput(SPACE_DESCRIPTION, SPACE_DESCRIPTION, null)
-                  .addValidator(StringLengthValidator.class, 0, 255));
-  }
+    private final String SPACE_NAME = "name";
+    private final String SPACE_PRIORITY = "priority";
+    private final String SPACE_DESCRIPTION = "description";
+
+    //These priority variables should be set in Space.java model
+    private final String PRIORITY_HIGH = "high";
+    private final String PRIORITY_INTERMEDIATE = "intermediate";
+    private final String PRIORITY_LOW = "low";
+
+    //Message
+    private final String MSG_INVALID_SPACE_NAME = "UISpaceSettings.msg.invalid_space_name";
+
+    /**
+     * constructor
+     *
+     * @param name
+     * @throws Exception
+     */
+    public UISpaceSettings(String name) throws Exception {
+        super(name);
+        addUIFormInput(new UIFormStringInput(SPACE_NAME, SPACE_NAME, null)
+                .addValidator(MandatoryValidator.class)
+                .addValidator(ExpressionValidator.class, "^[\\p{L}][\\p{L}._\\- \\d]+$", "ResourceValidator.msg.Invalid-char")
+                .addValidator(ExpressionValidator.class, "^[\\p{L}][\\p{ASCII}]+$", MSG_INVALID_SPACE_NAME)
+                .addValidator(StringLengthValidator.class, 3, 30));
+
+        List<SelectItemOption<String>> priorityList = new ArrayList<SelectItemOption<String>>(3);
+        SelectItemOption<String> pHight = new SelectItemOption<String>(PRIORITY_HIGH, Space.HIGH_PRIORITY);
+        SelectItemOption<String> pImmediate = new SelectItemOption<String>(PRIORITY_INTERMEDIATE, Space.INTERMEDIATE_PRIORITY);
+        SelectItemOption<String> pLow = new SelectItemOption<String>(PRIORITY_LOW, Space.LOW_PRIORITY);
+        priorityList.add(pHight);
+        priorityList.add(pImmediate);
+        priorityList.add(pLow);
+        pImmediate.setSelected(true);
+
+        UIFormSelectBox selectPriority = new UIFormSelectBox(SPACE_PRIORITY, SPACE_PRIORITY, priorityList);
+        addUIFormInput(selectPriority);
+        addUIFormInput(new UIFormTextAreaInput(SPACE_DESCRIPTION, SPACE_DESCRIPTION, null)
+                .addValidator(StringLengthValidator.class, 0, 255));
+    }
 }
--- portlet/space/src/main/java/social/portal/webui/component/space/UISpaceActivityPortlet.java
+++ portlet/space/src/main/java/social/portal/webui/component/space/UISpaceActivityPortlet.java
@@ -16,11 +16,8 @@
  */
 package social.portal.webui.component.space;
 
-import javax.portlet.PortletPreferences;
-
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
-import org.exoplatform.social.application.impl.DefaultSpaceApplicationHandler;
 import org.exoplatform.social.core.activitystream.ActivityManager;
 import org.exoplatform.social.core.activitystream.model.Activity;
 import org.exoplatform.social.core.identity.IdentityManager;
@@ -32,7 +29,6 @@ import org.exoplatform.social.space.SpaceUtils;
 import org.exoplatform.social.space.impl.SpaceIdentityProvider;
 import org.exoplatform.web.application.ApplicationMessage;
 import org.exoplatform.webui.application.WebuiRequestContext;
-import org.exoplatform.webui.application.portlet.PortletRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
 import org.exoplatform.webui.core.UIApplication;
@@ -40,90 +36,90 @@ import org.exoplatform.webui.core.UIPortletApplication;
 import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
 import org.exoplatform.webui.event.Event;
 import org.exoplatform.webui.event.EventListener;
-
 import social.portal.webui.component.UIDisplaySpaceActivities;
 import social.portal.webui.component.composer.UIComposer;
 
 /**
  * UISpaceActivityPortlet.java
- * <p>
+ * <p/>
  * Displaying space activities and its member's posts
  *
- * @author    <a href="http://hoatle.net">hoatle</a>
- * @since 	  Apr 6, 2010
+ * @author <a href="http://hoatle.net">hoatle</a>
  * @copyright eXo Platform SAS
+ * @since Apr 6, 2010
  */
 @ComponentConfig(
-  lifecycle = UIApplicationLifecycle.class,
-  template = "app:/groovy/portal/webui/space/UISpaceActivityPortlet.gtmpl",
-  events = {
-    @EventConfig (listeners = UISpaceActivityPortlet.PostMessageActionListener.class)
-  }
-) 
+        lifecycle = UIApplicationLifecycle.class,
+        template = "app:/groovy/portal/webui/space/UISpaceActivityPortlet.gtmpl",
+        events = {
+                @EventConfig(listeners = UISpaceActivityPortlet.PostMessageActionListener.class)
+        }
+)
 public class UISpaceActivityPortlet extends UIPortletApplication {
-  static private final Log LOG = ExoLogger.getLogger(UISpaceActivityPortlet.class);
-  private Space space_;
-  private UIDisplaySpaceActivities uiDisplaySpaceActivities_;
-  //TODO hoatle need to specify reasonable characters length here
-  private static final int MIN_CHARACTERS_REQUIRED = 0;
-  private static final int MAX_CHARACTERS_ALLOWED = 500;
-  /**
-   * constructor
-   */
-  public UISpaceActivityPortlet() throws Exception {
-    UIComposer uiComposer = addChild(UIComposer.class, null, null);
-    uiComposer.setStringLengthValidator(MIN_CHARACTERS_REQUIRED, MAX_CHARACTERS_ALLOWED);
-    uiDisplaySpaceActivities_ = addChild(UIDisplaySpaceActivities.class, null, null);
-    space_ = getSpaceService().getSpaceByUrl(SpaceUtils.getSpaceUrl());
-    uiDisplaySpaceActivities_.setSpace(space_);
-  }
+    static private final Log LOG = ExoLogger.getLogger(UISpaceActivityPortlet.class);
+    private Space space_;
+    private UIDisplaySpaceActivities uiDisplaySpaceActivities_;
+    //TODO hoatle need to specify reasonable characters length here
+    private static final int MIN_CHARACTERS_REQUIRED = 0;
+    private static final int MAX_CHARACTERS_ALLOWED = 500;
 
-  
-  public SpaceService getSpaceService() {
-    return getApplicationComponent(SpaceService.class);
-  }
-  
-  public Space getSpace() {
-    return space_;
-  }
-  
-  public void setSpace(Space space) {
-    space_ = space;
-  }
-  
-  /**
-   * resets to reload all activities
-   * @throws Exception
-   */
-  public void refresh() throws Exception {
-    uiDisplaySpaceActivities_.setSpace(space_);
-  }
-  
-  static public class PostMessageActionListener extends EventListener<UIComposer> {
-
-    @Override
-    public void execute(Event<UIComposer> event) throws Exception {
-      UIComposer uiComposer = event.getSource();
-      WebuiRequestContext requestContext = event.getRequestContext();
-      UIApplication uiApplication = requestContext.getUIApplication();
-      String bodyData  = uiComposer.getBodyData();
-      if (bodyData.equals("")) {
-        uiApplication.addMessage(new ApplicationMessage("UIComposer.msg.error.Empty_Message", null, ApplicationMessage.ERROR));
-        return;
-      }
-      String member = requestContext.getRemoteUser();
-      UISpaceActivityPortlet uiSpaceActivityPortlet = uiComposer.getAncestorOfType(UISpaceActivityPortlet.class);
-      UIDisplaySpaceActivities uiDisplaySpaceActivities = uiSpaceActivityPortlet.getChild(UIDisplaySpaceActivities.class);
-      Space space = uiSpaceActivityPortlet.getSpace();
-      uiComposer.reset();
-      ActivityManager activityManager = uiComposer.getApplicationComponent(ActivityManager.class);
-      IdentityManager identityManager = uiComposer.getApplicationComponent(IdentityManager.class);
-      Identity spaceIdentity = identityManager.getOrCreateIdentity(SpaceIdentityProvider.NAME, space.getId(), false);
-      Identity userIdentity = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, member);
-      Activity activity = new Activity(userIdentity.getId(), SpaceService.SPACES_APP_ID, space.getName(), bodyData);
-      activityManager.saveActivity(spaceIdentity, activity);
-      uiDisplaySpaceActivities.setSpace(space);
+    /**
+     * constructor
+     */
+    public UISpaceActivityPortlet() throws Exception {
+        UIComposer uiComposer = addChild(UIComposer.class, null, null);
+        uiComposer.setStringLengthValidator(MIN_CHARACTERS_REQUIRED, MAX_CHARACTERS_ALLOWED);
+        uiDisplaySpaceActivities_ = addChild(UIDisplaySpaceActivities.class, null, null);
+        space_ = getSpaceService().getSpaceByUrl(SpaceUtils.getSpaceUrl());
+        uiDisplaySpaceActivities_.setSpace(space_);
     }
-    
-  }
+
+
+    public SpaceService getSpaceService() {
+        return getApplicationComponent(SpaceService.class);
+    }
+
+    public Space getSpace() {
+        return space_;
+    }
+
+    public void setSpace(Space space) {
+        space_ = space;
+    }
+
+    /**
+     * resets to reload all activities
+     *
+     * @throws Exception
+     */
+    public void refresh() throws Exception {
+        uiDisplaySpaceActivities_.setSpace(space_);
+    }
+
+    static public class PostMessageActionListener extends EventListener<UIComposer> {
+
+        @Override
+        public void execute(Event<UIComposer> event) throws Exception {
+            UIComposer uiComposer = event.getSource();
+            WebuiRequestContext requestContext = event.getRequestContext();
+            UIApplication uiApplication = requestContext.getUIApplication();
+            String bodyData = uiComposer.getBodyData();
+            if (bodyData.equals("")) {
+                uiApplication.addMessage(new ApplicationMessage("UIComposer.msg.error.Empty_Message", null, ApplicationMessage.ERROR));
+                return;
+            }
+            String member = requestContext.getRemoteUser();
+            UISpaceActivityPortlet uiSpaceActivityPortlet = uiComposer.getAncestorOfType(UISpaceActivityPortlet.class);
+            UIDisplaySpaceActivities uiDisplaySpaceActivities = uiSpaceActivityPortlet.getChild(UIDisplaySpaceActivities.class);
+            Space space = uiSpaceActivityPortlet.getSpace();
+            uiComposer.reset();
+            ActivityManager activityManager = uiComposer.getApplicationComponent(ActivityManager.class);
+            IdentityManager identityManager = uiComposer.getApplicationComponent(IdentityManager.class);
+            Identity spaceIdentity = identityManager.getOrCreateIdentity(SpaceIdentityProvider.NAME, space.getId(), false);
+            Identity userIdentity = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, member);
+            Activity activity = new Activity(userIdentity.getId(), SpaceService.SPACES_APP_ID, space.getName(), bodyData);
+            activityManager.saveActivity(spaceIdentity, activity);
+            uiDisplaySpaceActivities.setSpace(space);
+        }
+    }
 }
