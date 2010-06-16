c82a940d04a37e3024a6c12b9134112b6789951e
Nguyen Trong Nghia <nghia.nguyen@exoplatform.com>
2010-06-16 08:15:26 +0000

    Add new QueryBuilder class supporting for creating SQL query. (UnitTest included)

---
+++ component/people/src/test/java/org/exoplatform/social/core/JCRTuts/ZAbstractTest.java
@@ -0,0 +1,28 @@
+package org.exoplatform.social.core.JCRTuts;
+
+import org.exoplatform.component.test.AbstractKernelTest;
+import org.exoplatform.component.test.ConfigurationUnit;
+import org.exoplatform.component.test.ConfiguredBy;
+import org.exoplatform.component.test.ContainerScope;
+
+/**
+ * AbstractPeopleTest.java:
+ *
+ * @author  <a href="http://hoatle.net">hoatle</a>
+ * @since   May 27, 2010 4:08:56 PM
+ */
+@ConfiguredBy({
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.test.jcr-configuration.xml"),
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.test.organization-configuration.xml")  
+})
+public abstract class ZAbstractTest extends AbstractKernelTest {
+  @Override
+  protected void setUp() throws Exception {
+    begin();
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    end();
+  }
+}
