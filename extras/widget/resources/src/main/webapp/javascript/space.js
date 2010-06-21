1a67233960d248c198b71be9ed9cbeee790672f6
Jeremi  Joslin <jeremi.joslin@exoplatform.com>
2010-06-21 04:36:55 +0000

    SOC-944: Integrate eXo Spaces with other website usign a widget

---
+++ extras/widget/resources/src/main/webapp/javascript/space.js
@@ -0,0 +1,161 @@
+/*jslint browser:true, nomen:false, evil:true */
+/*global alert: false, confirm: false, console: false, prompt: false, window: true */
+(function () {
+    //We set default configuration values
+    var configuration = {
+        //By default we use the URL as spaceID
+        spaceName: document.location.toString(),
+        //Do not include a trailing /
+        serverURL: "http://192.168.2.100:8080",
+        //start by a /
+        spaceServicePath: "/rest/private/spaces/portal/",
+        linkElId: "exoSpacesLink",
+        spaceInfoTmpl: '<div style="height:200px;overflow:hidden;width:220px;border:medium none;"><a href="javascript:void(0)" rel="close" style="float:right;">Close</a><br /><%= iframe %></div>'
+    },
+    templateRenderer;
+    
+    function getIframe(spaceName, description) {
+        return '<iframe  scrolling="no" height="180" frameborder="no" width="220" src="' + configuration.serverURL + configuration.spaceServicePath + "space_info?spaceName=" + encodeURIComponent(spaceName) + '&description=' + encodeURIComponent(description) + '"></iframe>'
+    }
+    
+    function createSpaceBox(container, spaceName, description) {
+        if (typeof container === "string") {
+            container = document.getElementById(container);
+        }
+        container.innerHTML = getIframe(spaceName, description);
+    }
+    
+    function getTemplateGenerator(template) {
+        //from ejohn http://ejohn.org/blog/javascript-micro-templating/ 
+        //MIT Licence
+        return new Function("obj",
+                "var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+                // Introduce the data as local variables using with(){}
+                "with(obj){p.push('" +
+
+                // Convert the template into pure JavaScript
+                template
+                  .replace(/[\r\t\n]/g, " ")
+                  .split("<%").join("\t")
+                  .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+                  .replace(/\t=(.*?)%>/g, "',$1,'")
+                  .split("\t").join("');")
+                  .split("%>").join("p.push('")
+                  .split("\r").join("\\'") + "');}return p.join('');");
+    }
+    
+    function hideInfo() {
+        if (infoEl) {
+            infoEl.style.display = "none";
+        }
+    }
+    
+    
+    function bind(el, eventName, fn) {
+        el = (typeof(el) === 'string') ? document.getElementById.get(el) : el;
+        if (window.addEventListener) {  
+            el.addEventListener(eventName, fn, false);
+        }
+        if (window.attachEvent) {
+            el.attachEvent('on' + eventName, fn);
+        }
+    }
+    
+    function showInfo(link, spaceName, description) {
+        var html = [],
+            infoEl = document.getElementById("espInfoBox_" + encodeURIComponent(spaceName));
+        //console.log("infoEl", infoEl);
+        
+        if (!infoEl) {
+            
+            if (!templateRenderer) {
+                templateRenderer = getTemplateGenerator(configuration.spaceInfoTmpl);
+            }
+            
+            infoEl = document.createElement('div');
+            infoEl.className = "espInfoBox";
+            infoEl.id = "espInfoBox_" + encodeURIComponent(spaceName);
+            infoEl.style.top = "26px";
+            infoEl.style.left = "-4px";
+            infoEl.style["z-index"] = "10001";
+            
+            infoEl.innerHTML = templateRenderer({
+                iframe: getIframe(spaceName, description)
+            });
+            link.parentNode.insertBefore(infoEl, link.nextSibling);
+            
+            els = infoEl.getElementsByTagName("*");
+            for (i = 0, len = els.length; i < len; i += 1) {
+                if (els[i].getAttribute("rel") === "close") {
+                    bind(els[i], "click", function() {
+                        infoEl.style.display = "none";
+                    });
+                }
+            }
+        }
+        infoEl.style.display = "block";
+    }
+    
+    
+    function createPopup(link, spaceName, description) {
+        var stylesheetLink, scriptEl;
+        //Insert our stylesheet before our script
+        if (!document.getElementById("exoSpacesStylesheet")) {
+            stylesheetLink = document.createElement('link');
+            stylesheetLink.type = 'text/css';
+            stylesheetLink.rel = 'stylesheet';
+            stylesheetLink.href = configuration.serverURL + '/socialWidgetResources/skin/Stylesheet.css';
+            scriptEl = document.getElementsByTagName('script')[0]; 
+            scriptEl.parentNode.insertBefore(stylesheetLink, scriptEl);
+        }
+        
+        if (typeof link === "string") {
+            link = document.getElementById(link);
+        }
+        
+        
+        if (link.tagName === "A") {
+            link.href = configuration.serverURL + configuration.spaceServicePath + "go_to_space?spaceName=" + encodeURIComponent(spaceName) + '&description=' + encodeURIComponent(description);
+        }
+        
+        link.onclick = function(e) {
+            showInfo(link, spaceName, description);
+            
+            if (!e) { 
+                e = window.event;
+            }
+            e.cancelBubble = true;
+            if (e.stopPropagation) {
+                e.stopPropagation();
+            }
+            return false;
+        };
+    }
+    
+    function initialize() {
+        var i,
+            _esp = window["_esp"] || [],
+            param;
+        
+        //If we have some configuration provided we set it
+        if (_esp) {
+            for (i = 0; i < _esp.length; i += 1) {
+                param = _esp[i];
+                if (param[0] === "createSpaceBox") {
+                    createSpaceBox(param[1], param[2], param[3]);
+                } else if (param[0] === "createPopup") {
+                    createPopup(param[1], param[2], param[3]);
+                } else {
+                    configuration[param[0]] = param[1];
+                }
+                
+            }
+        }
+    }
+    
+    window.spaces = window.spaces || {};
+    window.spaces.createSpaceBox = createSpaceBox;
+    window.spaces.createPopup = createPopup;
+    initialize();
+}());
