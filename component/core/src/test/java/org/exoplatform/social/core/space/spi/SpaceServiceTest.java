f4db869d3d87e0966ce2e741d3743ec20612ed8a
Hanh Vi <hanh.vi@exoplatform.com>
2010-08-06 03:09:57 +0000

    SOC-961 Code coverage for other project

--- component/core/src/test/java/org/exoplatform/social/core/space/spi/SpaceServiceTest.java
+++ component/core/src/test/java/org/exoplatform/social/core/space/spi/SpaceServiceTest.java
@@ -16,8 +16,11 @@
  */
 package org.exoplatform.social.core.space.spi;
 
+import java.util.List;
+
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
+import org.exoplatform.social.core.space.SpaceUtils;
 import org.exoplatform.social.core.space.model.Space;
 import org.exoplatform.social.core.test.AbstractCoreTest;
 
@@ -38,98 +41,139 @@ public class SpaceServiceTest extends AbstractCoreTest {
     super.tearDown();
   }
 
-  public void testGetAllSpaces() {
-    assertTrue(true);
+  public void testGetAllSpaces() throws Exception {
+    populateData();
+    createMoreSpace("Space2");
+    assertEquals(2, spaceService.getAllSpaces().size());
+    destroyData();
   }
 
-  public void testGetSpaceByName() {
-    assertTrue(true);
+  public void testGetSpaceByName() throws Exception {
+    Space space = populateData();
+    assertEquals(space.getName(), spaceService.getSpaceByName("Space1").getName());
+    destroyData();
   }
 
-  public void testGetSpacesByFirstCharacterOfName() {
-    assertTrue(true);
+  public void testGetSpacesByFirstCharacterOfName() throws Exception {
+    populateData();
+    createMoreSpace("Space2");
+    assertEquals(2, spaceService.getSpacesByFirstCharacterOfName("S").size());
+    destroyData();
   }
 
-  public void testGetSpacesBySearchCondition() {
-    assertTrue(true);
+  public void testGetSpacesBySearchCondition() throws Exception {
+    populateData();
+    createMoreSpace("Space2");
+    assertEquals(2, spaceService.getSpacesBySearchCondition("Space").size());
+    assertEquals(1, spaceService.getSpacesBySearchCondition("1").size());
+    destroyData();
   }
 
-  public void testGetSpaceById() {
-    assertTrue(true);
+  public void testGetSpaceById() throws Exception {
+    Space space = populateData();
+    createMoreSpace("Space2");
+    assertEquals(space.getName(), spaceService.getSpaceById(space.getId()).getName());
+    destroyData();
   }
 
-  public void testGetSpaceByUrl() {
-    assertTrue(true);
+  public void testGetSpaceByUrl() throws Exception {
+    Space space = populateData();
+    assertEquals(space.getName(), spaceService.getSpaceByUrl("space1").getName());
+    destroyData();
+  }
+  
+// //Relate to navigation and page maybe tested later 
+//  public void testGetSpaces() throws Exception {
+//    Space space = populateData();
+//    spaceService.addMember(space, "root");
+//    assertEquals(1, spaceService.getSpaces("root").size());
+//    destroyData();
+//  }
+//
+//  public void testGetAccessibleSpaces() throws Exception {
+//    populateData();
+//    Space space = spaceService.getSpaceByName("Space1");
+//    spaceService.setLeader(space, "root", true);
+//    assertEquals(1, spaceService.getAccessibleSpaces("root").size());
+//    destroyData();
+//  }
+
+  public void testGetEditableSpaces() throws Exception {
+    populateData();
+    assertEquals(1, spaceService.getEditableSpaces("root").size());
+    destroyData();
   }
 
-  public void testGetSpaces() {
-    assertTrue(true);
+  public void testGetInvitedSpaces() throws Exception {
+    populateData();
+    assertEquals(0, spaceService.getInvitedSpaces("root").size());
+    Space space = spaceService.getSpaceByName("Space1");
+    spaceService.inviteMember(space, "root");
+    assertEquals(1, spaceService.getInvitedSpaces("root").size());
+    destroyData();
   }
 
-  public void testGetAccessibleSpaces() {
-    assertTrue(true);
+  public void testGetPublicSpaces() throws Exception {
+    populateData();
+    assertEquals(1, spaceService.getPublicSpaces("root").size());
+    destroyData();
   }
 
-  public void testGetEditableSpaces() {
-    assertTrue(true);
-  }
-
-  public void testGetInvitedSpaces() {
-    assertTrue(true);
-  }
-
-  public void testGetPublicSpaces() {
-   assertTrue(true);
-  }
-
-  public void testGetPendingSpaces() {
-    assertTrue(true);
+  public void testGetPendingSpaces() throws Exception {
+    populateData();
+    Space space = spaceService.getSpaceByName("Space1");
+    spaceService.requestJoin(space, "root");
+    assertEquals(true, spaceService.isPending(space, "root"));
+    destroyData();
   }
 
   public void testCreateSpace() throws Exception {
-    assertTrue(true);
-    /*
+    populateData();
+    createMoreSpace("Space2");
+    assertEquals(2, spaceService.getAllSpaces().size());
+    destroyData();
+  }
+
+  private Space populateData() throws Exception {
+    String spaceName = "Space1";
     Space space1 = new Space();
     space1.setApp("Calendar;FileSharing");
-    space1.setGroupId("Group1");
-    space1.setName("Space1");
-
-    Space space2 = new Space();
-    space2.setApp("Contact,Forum");
-    space2.setGroupId("Group2");
-    space2.setName("Space2");
+    space1.setName(spaceName);
+    String shortName = SpaceUtils.cleanString(spaceName);
+    space1.setGroupId("/spaces/" + shortName);
+    space1.setUrl(shortName);
+    space1.setRegistration("validation");
+    space1.setDescription("This is my first space for testing"); 
+    space1.setType("classic");
+    space1.setVisibility("public");
+    space1.setPriority("2");
 
     spaceService.saveSpace(space1, true);
+    return space1;
+  }
+  
+  private Space createMoreSpace(String spaceName) throws Exception {
+    Space space2 = new Space();
+    space2.setApp("Contact,Forum");
+    space2.setName(spaceName);
+    String shortName = SpaceUtils.cleanString(spaceName);
+    space2.setGroupId("/spaces/" + shortName );
+    space2.setUrl(shortName);
+    space2.setRegistration("open");
+    space2.setDescription("This is my second space for testing"); 
+    space2.setType("classic");
+    space2.setVisibility("public");
+    space2.setPriority("2");
+    
     spaceService.saveSpace(space2, true);
-
-    assertEquals(2, spaceService.getAllSpaces().size());
-
-    Space space3 = spaceService.getSpaceById(space1.getId());
-    assertNotNull(space3);
-    assertEquals(space1.getId(), space3.getId());
-    assertNotSame(space3.getApp(), "Calendar");
-    assertEquals(space3.getGroupId(), "Group1");
-
-    spaceService.deleteSpace(space1);
-    spaceService.deleteSpace(space2);
-    */
+    
+    return space2;
   }
-
-  public void testGetSpace() throws Exception {
-    assertTrue(true);
-    /*
-    Space space = new Space();
-    space.setApp("Calendar");
-    space.setGroupId("Group1");
-    space.setName("calendar");
-    spaceService.saveSpace(space, true);
-    Space gotSpace = spaceService.getSpaceById(space.getId());
-    assertNotNull(gotSpace);
-    assertEquals(1, spaceService.getAllSpaces().size());
-    assertEquals("Calendar", gotSpace.getApp());
-    assertEquals("Group1", gotSpace.getGroupId());
-    spaceService.deleteSpace(gotSpace);
-    */
+  
+  private void destroyData() throws Exception {
+    List<Space> spaces = spaceService.getAllSpaces();
+    for (Space space : spaces) {
+      spaceService.deleteSpace(space);
+    }
   }
-
 }
