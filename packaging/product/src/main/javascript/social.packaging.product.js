6383959b0857ce3f713e95575aea38a70aeea647
Hoat Le <hoat.le@exoplatform.com>
2010-08-23 08:48:47 +0000

    SOC-1043 | Social extension forces the creation of the classic portal

--- packaging/product/src/main/javascript/social.packaging.product.js
+++ packaging/product/src/main/javascript/social.packaging.product.js
@@ -38,6 +38,7 @@ function getProduct(version) {
   product.addDependencies(social.webapp.opensocial) ;
   product.addDependencies(social.webapp.portlet);
   product.addDependencies(social.webapp.resources);
+  product.addDependencies(social.webapp.socialExtensionPortal);
   product.addDependencies(social.extras.feedmash);
   product.addDependencies(social.extras.linkComposerPlugin);
   product.addDependencies(social.extension.war) ;
