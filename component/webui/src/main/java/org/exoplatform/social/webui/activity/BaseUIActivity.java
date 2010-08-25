cad4ab31f76942483b267e7f608be375fbf55869
Nguyen Trong Nghia <nghia.nguyen@exoplatform.com>
2010-08-25 09:09:49 +0000

    SOC-1056 : Click on author in user activity title links to "classic"

--- component/webui/src/main/java/org/exoplatform/social/webui/activity/BaseUIActivity.java
+++ component/webui/src/main/java/org/exoplatform/social/webui/activity/BaseUIActivity.java
@@ -368,7 +368,7 @@ public class BaseUIActivity extends UIForm {
     }
 
     LinkProvider linkProvider = (LinkProvider) PortalContainer.getInstance().getComponentInstanceOfType(LinkProvider.class);
-    return linkProvider.getProfileLink(userIdentity.getRemoteId(), OrganizationIdentityProvider.NAME);      
+    return linkProvider.getProfileLink(userIdentity.getRemoteId(), null);      
   }
 
   /**
