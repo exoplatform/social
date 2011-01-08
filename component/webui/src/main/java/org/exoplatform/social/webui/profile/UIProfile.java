b57f07245833e382bca19b20fe1cfe81a26a5b20
Hoat Le <hoat.le@exoplatform.com>
2011-01-08 16:44:24 +0000

    SOC-1423 | Should change to use UIContainerLifecycle instead of UIApplicationLifecycle for extending UIContainer: Do not use lifecycle

--- component/webui/src/main/java/org/exoplatform/social/webui/profile/UIProfile.java
+++ component/webui/src/main/java/org/exoplatform/social/webui/profile/UIProfile.java
@@ -31,7 +31,6 @@ import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
 import org.exoplatform.webui.core.UIContainer;
 import org.exoplatform.webui.core.UIPopupWindow;
-import org.exoplatform.webui.core.lifecycle.UIContainerLifecycle;
 import org.exoplatform.webui.event.Event;
 import org.exoplatform.webui.event.EventListener;
 
@@ -42,7 +41,6 @@ import org.exoplatform.webui.event.EventListener;
  */
 
 @ComponentConfig(
-  lifecycle = UIContainerLifecycle.class,
   template = "classpath:groovy/social/webui/profile/UIProfile.gtmpl",
   events = {
     @EventConfig(listeners=UIProfile.ChangeAvatarActionListener.class)
