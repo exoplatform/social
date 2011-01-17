606d6f48a3c42ffa1d98bd67f8b618fa91238ba6
Hoat Le <hoat.le@exoplatform.com>
2011-01-17 21:10:54 +0000

    SOC-1414 | Sometimes profile display [null] value for username: add @since, check null value

--- component/core/src/main/java/org/exoplatform/social/core/service/LinkProvider.java
+++ component/core/src/main/java/org/exoplatform/social/core/service/LinkProvider.java
@@ -279,6 +279,9 @@ public class LinkProvider {
    * @return
    */
   private static String escapeJCRSpecialCharacters(String string) {
+    if (string == null) {
+      return null;
+    }
     return string.replace("[", "%5B").replace("]", "%5D").replace(":", "%3A");
   }
 
@@ -315,8 +318,8 @@ public class LinkProvider {
   /**
    * Gets portal owner, if parameter is null or "", the method return default portal owner
    * 
-   * @param portalOwner
-   * @return portalOwner
+   * @param portalName
+   * @return portalName
    */
   private static String getPortalName(String portalName) {
     if (portalName == null || "".equals(portalName)) {
