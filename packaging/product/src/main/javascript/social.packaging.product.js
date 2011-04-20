9fa517da35a37f95cf3023eca45c475da1d65220
Hoat Le <hoat.le@exoplatform.com>
2011-04-20 09:23:59 +0000

    SOC-1703 | Upgrade to use JCR 1.14.0-CR01: Remove hard-coded things in social.packaging.product.js

--- packaging/product/src/main/javascript/social.packaging.product.js
+++ packaging/product/src/main/javascript/social.packaging.product.js
@@ -51,25 +51,25 @@ function getProduct(version) {
   product.addServerPatch("jboss",  portal.server.jboss.patch);
   product.addServerPatch("jbossear",  social.server.jbossear.patch);
 
-  product.removeDependency(new Project("org.exoplatform.jcr", "exo.jcr.component.ftp", "jar", eXoJcr.version));
+  //product.removeDependency(new Project("org.exoplatform.jcr", "exo.jcr.component.ftp", "jar", eXoJcr.version));
 
   //upgrade lib for opensocial
-  product.removeDependency(new Project("commons-beanutils", "commons-beanutils", "jar", "1.6"));
-  product.removeDependency(new Project("commons-collections", "commons-collections", "jar", "2.1"));
-  product.removeDependency(new Project("commons-collections", "commons-collections", "jar", "3.1"));
-  product.removeDependency(new Project("commons-digester", "commons-digester", "jar", "1.6"));
-  product.removeDependency(new Project("commons-httpclient", "commons-httpclient", "jar", "3.0"));
-  product.removeDependency(new Project("xstream", "xstream", "jar", "1.0.2"));
+  //product.removeDependency(new Project("commons-beanutils", "commons-beanutils", "jar", "1.6"));
+  //product.removeDependency(new Project("commons-collections", "commons-collections", "jar", "2.1"));
+  //product.removeDependency(new Project("commons-collections", "commons-collections", "jar", "3.1"));
+  //product.removeDependency(new Project("commons-digester", "commons-digester", "jar", "1.6"));
+  //product.removeDependency(new Project("commons-httpclient", "commons-httpclient", "jar", "3.0"));
+  //product.removeDependency(new Project("xstream", "xstream", "jar", "1.0.2"));
   //product.removeDependency(new Project("org.exoplatform.core", "exo.core.component.document", "jar", core.version));
   //product.removeDependency(new Project("org.exoplatform.core", "exo.core.component.xml-processing", "jar", core.version));
 
 
-  product.addDependencies(new Project("commons-beanutils", "commons-beanutils", "jar", "1.7.0"));
-  product.addDependencies(new Project("commons-beanutils", "commons-beanutils-core", "jar", "1.7.0"));
-  product.addDependencies(new Project("commons-digester", "commons-digester", "jar", "1.7"));
-  product.addDependencies(new Project("commons-httpclient", "commons-httpclient", "jar", "3.1"));
+  //product.addDependencies(new Project("commons-beanutils", "commons-beanutils", "jar", "1.7.0"));
+  //product.addDependencies(new Project("commons-beanutils", "commons-beanutils-core", "jar", "1.7.0"));
+  //product.addDependencies(new Project("commons-digester", "commons-digester", "jar", "1.7"));
+  //product.addDependencies(new Project("commons-httpclient", "commons-httpclient", "jar", "3.1"));
 
-  product.addDependencies(new Project("findbugs", "annotations", "jar", "1.0.0"));
+  //product.addDependencies(new Project("findbugs", "annotations", "jar", "1.0.0"));
 
   product.module = social ;
   product.dependencyModule = [kernel, core, ws, eXoJcr, portal];
