d252956ca7c8f6673637f3156e24719405b0f03c
Hoat Le <hoat.le@exoplatform.com>
2011-08-11 07:59:05 +0000

    SOC-1719 | OSHtmlProcessor implementation: Fix unit test

--- component/core/src/test/java/org/exoplatform/social/core/processor/OSHtmlSanitizerProcessorTest.java
+++ component/core/src/test/java/org/exoplatform/social/core/processor/OSHtmlSanitizerProcessorTest.java
@@ -73,7 +73,7 @@ public class OSHtmlSanitizerProcessorTest extends AbstractCoreTest {
     sample = "<script href='#' />bar</a>";
     activity.setTitle(sample);
     processor.processActivity(activity);
-    assertEquals("&lt;script href=&quot;#&quot;&gt;&lt;/script&gt;bar&lt;/a&gt;", activity.getTitle());
+    assertEquals("&lt;script href=&quot;#&quot; /&gt;bar&lt;/a&gt;", activity.getTitle());
 
     // forbidden tag
     sample = "<script>foo</script>";
