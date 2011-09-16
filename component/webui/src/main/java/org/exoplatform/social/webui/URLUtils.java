be6c809d6760ce73aca40d70c260e495bcd624fe
Hanh Vi <hanh.vi@exoplatform.com>
2011-09-16 08:20:57 +0000

    SOC-2086 Activity gadget does not work when add to homepage.

--- component/webui/src/main/java/org/exoplatform/social/webui/URLUtils.java
+++ component/webui/src/main/java/org/exoplatform/social/webui/URLUtils.java
@@ -20,6 +20,8 @@ import org.exoplatform.container.ExoContainer;
 import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.portal.application.PortalRequestContext;
 import org.exoplatform.portal.webui.util.Util;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 import org.exoplatform.social.core.identity.model.Identity;
 import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
 import org.exoplatform.social.core.manager.IdentityManager;
@@ -31,6 +33,8 @@ import org.exoplatform.web.controller.QualifiedName;
  */
 public class URLUtils {
 
+  private static Log LOG = ExoLogger.getLogger(UISocialGroupSelector.class);
+  
   /**
    * Gets current user name base on analytic the current url.<br>
    * 
@@ -43,11 +47,17 @@ public class URLUtils {
     String currentPath = request.getControllerContext().getParameter(QualifiedName.parse("gtn:path"));
     String []splitCurrentUser = currentPath.split("/");
     String currentUserName = currentPath.split("/")[splitCurrentUser.length - 1];
-    if (currentUserName != null) {
-      Identity id = idm.getOrCreateIdentity(OrganizationIdentityProvider.NAME, currentUserName, false);
-      if (id != null) return currentUserName;
+    try {
+      if (currentUserName != null) {
+        Identity id = idm.getOrCreateIdentity(OrganizationIdentityProvider.NAME, currentUserName, false);
+        if (id != null) return currentUserName;
+      }
+    } catch (Exception e) {
+      if(LOG.isWarnEnabled()) {
+        LOG.warn("Could not found Identity of user " + currentUserName);
+      }
+      return null;
     }
-    
     return null;
   }
 }
