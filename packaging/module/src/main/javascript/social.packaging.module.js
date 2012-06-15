2da0395022a408998fd5e40abf41ea7ecada4863
Vi Quoc Hanh <hanh.vi@exoplatform.com>
2012-06-15 13:14:02 +0000

    SOC-2691 | STB-0014 - improve all artifactsID names and organization.

--- packaging/module/src/main/javascript/social.packaging.module.js
+++ packaging/module/src/main/javascript/social.packaging.module.js
@@ -21,27 +21,27 @@ function getModule(params) {
 
   module.component = {} ;
   module.component.common =
-    new Project("org.exoplatform.social", "exo.social.component.common","jar", module.version);
+    new Project("org.exoplatform.social", "social-component-common","jar", module.version);
 
   module.component.core =
-  new Project("org.exoplatform.social", "exo.social.component.core","jar", module.version);
+  new Project("org.exoplatform.social", "social-component-core","jar", module.version);
 
   module.component.service =
-  new Project("org.exoplatform.social", "exo.social.component.service","jar", module.version).
+  new Project("org.exoplatform.social", "social-component-service","jar", module.version).
     addDependency(ws.frameworks.json).
     addDependency(new Project("org.exoplatform.commons", "exo.platform.commons.webui", "jar", platformCommonsVersion));
 
 
   module.component.opensocial =
-  new Project("org.exoplatform.social", "exo.social.component.opensocial","jar", module.version);
+  new Project("org.exoplatform.social", "social-component-opensocial","jar", module.version);
 
   module.component.webui =
-  new Project("org.exoplatform.social", "exo.social.component.webui","jar", module.version).
+  new Project("org.exoplatform.social", "social-component-webui","jar", module.version).
       addDependency(new Project("org.exoplatform.commons", "exo.platform.commons.webui.ext", "jar", platformCommonsVersion));
 
   module.webapp = {};
 
-  module.webapp.opensocial = new Project("org.exoplatform.social", "exo.social.webapp.opensocial", "war", module.version);
+  module.webapp.opensocial = new Project("org.exoplatform.social", "social-webapp-opensocial", "war", module.version);
   /*
     addDependency(new Project("commons-betwixt", "commons-betwixt", "jar", "0.8")).
     addDependency(new Project("net.sf.json-lib", "json-lib", "jar", "2.2")).
@@ -50,54 +50,54 @@ function getModule(params) {
   */
   module.webapp.opensocial.deployName = "social";
 
-  module.webapp.portlet = new Project("org.exoplatform.social", "exo.social.webapp.portlet", "war", module.version);
+  module.webapp.portlet = new Project("org.exoplatform.social", "social-webapp-portlet", "war", module.version);
   module.webapp.portlet.deployName = "social-portlet";
 
-  module.webapp.resources = new Project("org.exoplatform.social", "exo.social.webapp.resources", "war", module.version);
+  module.webapp.resources = new Project("org.exoplatform.social", "social-webapp-resources", "war", module.version);
   module.webapp.resources.deployName = "social-resources";
 
-  //module.webapp.socialExtensionPortal = new Project("org.exoplatform.social", "exo.social.webapp.social-extension-portal", "war", module.version);
+  //module.webapp.socialExtensionPortal = new Project("org.exoplatform.social", "social-webapp-social-extension-portal", "war", module.version);
   //module.webapp.socialExtensionPortal.deployName = "social-extension-portal";
 
   module.extras = {};
-  module.extras.feedmash = new Project("org.exoplatform.social", "exo.social.extras.feedmash", "jar", module.version);
-  module.extras.linkComposerPlugin = new Project("org.exoplatform.social", "exo.social.extras.link-composer-plugin", "jar", module.version);
+  module.extras.feedmash = new Project("org.exoplatform.social", "social-extras-feedmash", "jar", module.version);
+  module.extras.linkComposerPlugin = new Project("org.exoplatform.social", "social-extras-link-composer-plugin", "jar", module.version);
 
-  module.extras.migration = new Project("org.exoplatform.social", "exo.social.extras.migration", "jar", module.version);
-  module.extras.updater = new Project("org.exoplatform.social", "exo.social.extras.updater", "jar", module.version);
+  module.extras.migration = new Project("org.exoplatform.social", "social-extras-migration", "jar", module.version);
+  module.extras.updater = new Project("org.exoplatform.social", "social-extras-updater", "jar", module.version);
 
-  module.extras.widgetRest = new Project("org.exoplatform.social", "exo.social.extras.widget.rest", "jar", module.version);
-  module.extras.widgetResources = new Project("org.exoplatform.social", "exo.social.extras.widget.resources", "war", module.version);
+  module.extras.widgetRest = new Project("org.exoplatform.social", "social-extras-widget-rest", "jar", module.version);
+  module.extras.widgetResources = new Project("org.exoplatform.social", "social-extras-widget-resources", "war", module.version);
   module.extras.widgetResources.deployName = "socialWidgetResources";
 
   module.extension = {};
 
   module.extension.war =
-   new Project("org.exoplatform.social", "exo.social.extension.war", "war", module.version).
-   addDependency(new Project("org.exoplatform.social", "exo.social.extension.config", "jar", module.version));
+   new Project("org.exoplatform.social", "social-extension-war", "war", module.version).
+   addDependency(new Project("org.exoplatform.social", "social-extension-config", "jar", module.version));
   module.extension.war.deployName = "social-extension";
 
   module.demo = {};
   // demo portal
   module.demo.portal =
-    new Project("org.exoplatform.social", "exo.social.demo.war", "war", module.version).
-    addDependency(new Project("org.exoplatform.social", "exo.social.demo.config", "jar", module.version));
+    new Project("org.exoplatform.social", "social-demo-war", "war", module.version).
+    addDependency(new Project("org.exoplatform.social", "social-demo-config", "jar", module.version));
   module.demo.portal.deployName = "socialdemo";
 
   // demo rest endpoint
   module.demo.rest =
-    new Project("org.exoplatform.social", "exo.social.demo.rest", "war", module.version);
+    new Project("org.exoplatform.social", "social-demo-rest", "war", module.version);
   module.demo.rest.deployName = "rest-socialdemo";
 
    module.server = {}
 
    module.server.tomcat = {}
    module.server.tomcat.patch =
-   new Project("org.exoplatform.social", "exo.social.server.tomcat.patch", "jar", module.version);
+   new Project("org.exoplatform.social", "social-server-tomcat-patch", "jar", module.version);
 
    module.server.jbossear = {}
    module.server.jbossear.patch =
-   new Project("org.exoplatform.social", "exo.social.server.jboss.patch-ear", "jar", module.version);
+   new Project("org.exoplatform.social", "social-server-jboss-patch-ear", "jar", module.version);
 
   return module;
 
