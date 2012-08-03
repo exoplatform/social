8dfc90094ba1ee9e39d0ca4c7f9b7ed17a4ba395
Vi Quoc Hanh <hanh.vi@exoplatform.com>
2012-08-03 14:23:08 +0000

    SOC-1572 Properly resize the icon for a space, and profile.
    Fix description: with uploaded image that have different size of width and height, then after resize, it will be cropped to make width and height have the same size.

--- component/core/src/main/java/org/exoplatform/social/core/image/ImageUtils.java
+++ component/core/src/main/java/org/exoplatform/social/core/image/ImageUtils.java
@@ -112,6 +112,7 @@ public class ImageUtils {
         LOG.warn("Fail to resize image to avatar attachment with dimention <= 0x0");
         return null;
       }
+      
       if (height <= minSize)
         height = image.getHeight() * width / image.getWidth();
       else if (width <= minSize)
@@ -119,15 +120,38 @@ public class ImageUtils {
 
       // Create temp file to store resized image to put to avatar attachment
       File tmp = File.createTempFile("RESIZED", null);
-      ImageIO.write(org.apache.shindig.gadgets.rewrite.image.ImageUtils.getScaledInstance(image,
+      image = org.apache.shindig.gadgets.rewrite.image.ImageUtils.getScaledInstance(image,
                                                  width,
                                                  height,
                                                  RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR,
                                                  false,
-                                                 BufferedImage.TYPE_INT_RGB),
-                    extension,
-                    tmp);
+                                                 BufferedImage.TYPE_INT_RGB);
 
+      int imgWidth = image.getWidth();
+      int imgHeight = image.getHeight();
+      
+      if (imgWidth != imgHeight) { // in case of image has width and height not the same
+        int x,y,w,h;
+        int cropDimension = imgWidth > imgHeight ? imgHeight : imgWidth;
+        int offset = (cropDimension == imgWidth) ? imgHeight - cropDimension : imgWidth - cropDimension;
+        
+        if (imgWidth < imgHeight) {
+          x = 0;
+          y = (int)(offset/2);
+          w = imgWidth;
+          h = imgHeight - offset;
+        } else {
+          x = (int)(offset/2);
+          y = 0;
+          w = imgWidth - offset;
+          h = imgHeight;
+        }
+        
+        image = image.getSubimage(x, y, w, h);
+      }
+      
+      ImageIO.write(image, extension, tmp);
+      
       // Create new avatar attachment
       AvatarAttachment newAvatarAttachment = new AvatarAttachment(avatarId,
                                                                   avatarFileName,
