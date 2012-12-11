5e0d8e0a26b350fb08cca38a79d5d33f7b7e0646
Vi Quoc Hanh <hanh.vi@exoplatform.com>
2012-12-11 10:13:08 +0000

    Fix bug cannot create Space and remove Priority

--- component/webui/src/main/java/org/exoplatform/social/webui/space/UISpaceAddForm.java
+++ component/webui/src/main/java/org/exoplatform/social/webui/space/UISpaceAddForm.java
@@ -45,12 +45,11 @@ import org.exoplatform.webui.core.lifecycle.UIFormLifecycle;
 import org.exoplatform.webui.event.Event;
 import org.exoplatform.webui.event.Event.Phase;
 import org.exoplatform.webui.event.EventListener;
-import org.exoplatform.webui.form.UIFormCheckBoxInput;
 import org.exoplatform.webui.form.UIFormInputInfo;
 import org.exoplatform.webui.form.UIFormInputSet;
 import org.exoplatform.webui.form.UIFormRadioBoxInput;
-import org.exoplatform.webui.form.UIFormSelectBox;
 import org.exoplatform.webui.form.UIFormTabPane;
+import org.exoplatform.webui.form.input.UICheckBoxInput;
 
 /**
  * UIAddSpaceForm to create new space. By using this UIForm, user can create a
@@ -65,7 +64,6 @@ import org.exoplatform.webui.form.UIFormTabPane;
   events = {
     @EventConfig(listeners = UISpaceAddForm.CreateActionListener.class),
     @EventConfig(listeners = UISpaceAddForm.ToggleUseGroupActionListener.class, phase = Phase.DECODE),
-    @EventConfig(listeners = UISpaceAddForm.ChangePriorityActionListener.class, phase = Phase.DECODE),
     @EventConfig(listeners = UISpaceAddForm.ChangeOptionActionListener.class, phase = Phase.DECODE)
   }
 )
@@ -85,7 +83,6 @@ public class UISpaceAddForm extends UIFormTabPane {
   static private final String MSG_ERROR_SPACE_ALREADY_EXIST = "UISpaceAddForm.msg.error_space_already_exist";
   private final String SPACE_SETTINGS = "UISpaceSettings";
   private final String SPACE_VISIBILITY = "UISpaceVisibility";
-  private final String CHANGE_PRIORITY = "ChangePriority";
 
   /**
    * Constructor: add 3 UI component to this UIFormTabPane:
@@ -101,8 +98,6 @@ public class UISpaceAddForm extends UIFormTabPane {
   public UISpaceAddForm() throws Exception {
     super("UISpaceAddForm");
     UIFormInputSet uiSpaceSettings = new UISpaceSettings(SPACE_SETTINGS);
-    UIFormSelectBox uiSelectBox = uiSpaceSettings.getChild(UIFormSelectBox.class);
-    uiSelectBox.setOnChange(CHANGE_PRIORITY);
     addChild(uiSpaceSettings);
 
     UIFormInputSet uiSpaceVisibility = new UISpaceVisibility(SPACE_VISIBILITY);
@@ -199,12 +194,11 @@ public class UISpaceAddForm extends UIFormTabPane {
    * poup will show up for choosing.
    */
   static public class ToggleUseGroupActionListener extends EventListener<UISpaceAddForm> {
-    @SuppressWarnings("unchecked")
     @Override
     public void execute(Event<UISpaceAddForm> event) throws Exception {
       UISpaceAddForm uiSpaceAddForm = event.getSource();
       UISpaceGroupBound uiSpaceGroupBound = uiSpaceAddForm.getChild(UISpaceGroupBound.class);
-      UIFormCheckBoxInput<Boolean> uiUseExistingGroup = uiSpaceGroupBound.getChild(UIFormCheckBoxInput.class);
+      UICheckBoxInput uiUseExistingGroup = uiSpaceGroupBound.getChild(UICheckBoxInput.class);
       if (uiUseExistingGroup.isChecked()) {
         UIPopupWindow uiPopup = uiSpaceGroupBound.getChild(UIPopupWindow.class);
         UISocialGroupSelector uiGroupSelector = uiSpaceAddForm.createUIComponent(UISocialGroupSelector.class,
@@ -216,45 +210,6 @@ public class UISpaceAddForm extends UIFormTabPane {
       } else {
         UIFormInputInfo uiFormInputInfo = uiSpaceGroupBound.getChild(UIFormInputInfo.class);
         uiFormInputInfo.setValue(null);
-      }
-    }
-  }
-
-  static public class ChangePriorityActionListener extends EventListener<UISpaceAddForm> {
-    private final String HIGH_PRIORITY_LABEL = "UISpaceSettings.label.HighPrio";
-    private final String INTERMEDIATE_PRIORITY_LABEL = "UISpaceSettings.label.InterMePrio";
-    private final String LOW_PRIORITY_LABEL = "UISpaceSettings.label.lowPrio";
-    
-    @Override
-    public void execute(Event<UISpaceAddForm> event) throws Exception {
-      WebuiRequestContext ctx = event.getRequestContext();
-      ResourceBundle resApp = ctx.getApplicationResourceBundle();
-      String highPrio = resApp.getString(HIGH_PRIORITY_LABEL);
-      String interMePrio = resApp.getString(INTERMEDIATE_PRIORITY_LABEL);
-      String lowPrio = resApp.getString(LOW_PRIORITY_LABEL);
-      
-      UISpaceAddForm uiSpaceAddForm = event.getSource();
-
-      UIFormInputSet uiSpaceSettings = uiSpaceAddForm.getChildById(uiSpaceAddForm.SPACE_SETTINGS);
-      
-      UIFormSelectBox selectedPriority = uiSpaceSettings.getChild(UIFormSelectBox.class);
-      
-      UIFormInputInfo uiFormInfo = uiSpaceSettings.getChild(UIFormInputInfo.class);
-      
-      int selectedValue = Integer.parseInt(selectedPriority.getValue());
-
-      switch (selectedValue) {
-        case 1:
-          uiFormInfo.setValue(highPrio);
-          break;
-       case 2:
-          uiFormInfo.setValue(interMePrio);
-          break;
-       case 3:
-          uiFormInfo.setValue(lowPrio);
-          break;
-       default:
-          break;
       }
     }
   }
