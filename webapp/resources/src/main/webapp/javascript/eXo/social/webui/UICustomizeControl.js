c243f16807e10cbcc2c2a55b10adb57e9af79175
thanhvc <thanhvc@exoplatform.com>
2013-03-27 10:12:01 +0700

    SOC-3104 | [Space] TypeError when setting Access and Edit values for a new space

--- webapp/resources/src/main/webapp/javascript/eXo/social/webui/UICustomizeControl.js
+++ webapp/resources/src/main/webapp/javascript/eXo/social/webui/UICustomizeControl.js
@@ -1,14 +1,17 @@
 (function($, uiForm) { 
-var UICustomizeControl = {
-  onLoad : function(uicomponentId) {
-		$('#' + uicomponentId).click(function() {
-		  $('input').each(function() {
-	      var form = $(this).closest('.UIForm');
-	      if (form != null ) uiForm.submitForm(form.attr("id"), 'ChangeOption', true);
-	    });
-	  });
-  }
-};
+	var UICustomizeControl = {
+	  onLoad : function(uicomponentId) {
+			$('#' + uicomponentId).find('input[type=radio]').
+				on('click', function() {
+					var uiForm = $(this).parents('.UIForm:first');
+					if (uiForm.length > 0) {
+						uiForm.submitForm(form.attr("id"), 'ChangeOption', true);					
+					}				
+				});
+			
+	
+	  }
+	};
 
-return UICustomizeControl;
+	return UICustomizeControl;
 })($, uiForm);
