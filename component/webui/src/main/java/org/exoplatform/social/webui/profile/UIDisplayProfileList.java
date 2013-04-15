a8ec15df3a3e059697a11a878c21b26275d4ae21
Vi Quoc Hanh <hanh.vi@exoplatform.com>
2013-04-15 12:43:50 +0000

    SOC-3305 | Cancel request button does not work.

--- component/webui/src/main/java/org/exoplatform/social/webui/profile/UIDisplayProfileList.java
+++ component/webui/src/main/java/org/exoplatform/social/webui/profile/UIDisplayProfileList.java
@@ -425,11 +425,13 @@ public class UIDisplayProfileList extends UIContainer {
       }
       
       uiAllPeople.setLoadAtEnd(false);
-      if (relationship != null && relationship.getStatus() == Relationship.Type.CONFIRMED) {
+      if (relationship.getStatus() == Relationship.Type.CONFIRMED) {
         Utils.getRelationshipManager().delete(relationship);
-        event.getRequestContext().addUIComponentToUpdateByAjax(uiAllPeople);
+      } else {
+        Utils.getRelationshipManager().deny(inviIdentityIdentity, invitingIdentity);
       }
-
+      
+      event.getRequestContext().addUIComponentToUpdateByAjax(uiAllPeople);
     }
   }
 
