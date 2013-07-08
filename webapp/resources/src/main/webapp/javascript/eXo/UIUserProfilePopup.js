992b6c0fba722902b5030fe4d809a4865d8ba037
Vi Quoc Hanh <hanh.vi@exoplatform.com>
2013-07-08 17:28:56 +0000

    PLF-5236 | Who Is Online portlet took a lot of time when user login.Fix Rule 08.

--- webapp/resources/src/main/webapp/javascript/eXo/UIUserProfilePopup.js
+++ webapp/resources/src/main/webapp/javascript/eXo/UIUserProfilePopup.js
@@ -282,30 +282,22 @@
 							            if (relationStatus == "pending") { // Viewing is not owner
 							                action = $('<div/>', {
 							                    "class": "connect btn btn-primary",
-							                    "text": "" + labels.Confirm,
+							                    "text": "" + labels.Connect,
 							                    "data-action": "Accept:" + ownerUserId,
 							                    "onclick": "takeAction(this)"
 							                });
 							            } else if (relationStatus == "waiting") { // Viewing is owner
 							                action = $('<div/>', {
-							                    "class": "connect btn",
-							                    "text": "" + labels.CancelRequest,
-							                    "data-action": "Revoke:" + ownerUserId,
-							                    "onclick": "takeAction(this)"
+							                    "class": "",
+							                    "text": "" + labels.youHaveSentAnInvitation
 							                });
 							            } else if (relationStatus == "confirmed") { // Had Connection 
 							                action = $('<div/>', {
-							                    "class": "connect btn",
-							                    "text": "" + labels.RemoveConnection,
-							                    "data-action": "Disconnect:" + ownerUserId,
-							                    "onclick": "takeAction(this)"
+							                    "class": ""
 							                });
 							            } else if (relationStatus == "ignored") { // Connection is removed
 							                action = $('<div/>', {
-							                    "class": "connect btn",
-							                    "text": "" + labels.Ignore,
-							                    "data-action": "Deny:" + ownerUserId,
-							                    "onclick": "takeAction(this)"
+							                    "class": ""
 							                });
 							            }
 							    
