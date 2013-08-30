e32bc65899f6fc3b59c4aa594e0efb25068684ae
thanhvc <thanhvc@exoplatform.com>
2013-08-30 11:05:52 +0700

    SOC-3720 | CLONE - Activity Stream order are not correct after "Load More"

--- component/core/src/main/java/org/exoplatform/social/core/storage/impl/ActivityStreamStorageImpl.java
+++ component/core/src/main/java/org/exoplatform/social/core/storage/impl/ActivityStreamStorageImpl.java
@@ -143,7 +143,7 @@ public class ActivityStreamStorageImpl extends AbstractStorage implements Activi
       //
       ActivityEntity activityEntity = _findById(ActivityEntity.class, streamCtx.getActivity().getId());     
       if (OrganizationIdentityProvider.NAME.equals(owner.getProviderId())) {
-    createOwnerRefs(owner, activityEntity);
+        createOwnerRefs(owner, activityEntity);
       } else if (SpaceIdentityProvider.NAME.equals(owner.getProviderId())) {
         //
         manageRefList(new UpdateContext(owner, null), activityEntity, ActivityRefType.SPACE_STREAM);
@@ -774,6 +774,7 @@ public class ActivityStreamStorageImpl extends AbstractStorage implements Activi
 
     builder.where(whereExpression.toString());
     builder.orderBy(ActivityRef.lastUpdated.getName(), Ordering.DESC);
+    builder.orderBy(JCRProperties.name.getName(), Ordering.DESC);
     return builder.get().objects(offset, limit);
   }
   
