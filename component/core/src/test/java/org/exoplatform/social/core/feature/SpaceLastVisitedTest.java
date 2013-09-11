ca110d791182f084d4167fc4ce923869b25bbbcc
Nam Phung <nampq@exoplatform.com>
2013-09-11 15:03:37 +0700

    SOC-3751 | CLONE - Slowness in displaying Space activities

--- component/core/src/test/java/org/exoplatform/social/core/feature/SpaceLastVisitedTest.java
+++ component/core/src/test/java/org/exoplatform/social/core/feature/SpaceLastVisitedTest.java
@@ -105,7 +105,7 @@ public class SpaceLastVisitedTest extends AbstractCoreTest {
     spaces = spaceService.getLastAccessedSpace("mary", "app1", 0, 5);
     assertEquals(5, spaces.size());
     Space got = spaces.get(0);
-    assertEquals("space_4", got.getPrettyName());
+    assertEquals("space_1", got.getPrettyName());
     
     //
     Space space2 = spaceService.getSpaceByPrettyName("space_2");
@@ -114,7 +114,7 @@ public class SpaceLastVisitedTest extends AbstractCoreTest {
     spaces = spaceService.getLastAccessedSpace("mary", "app1", 0, 5);
     assertEquals(5, spaces.size());
     got = spaces.get(0);
-    assertEquals("space_2", got.getPrettyName());
+    assertEquals("space_1", got.getPrettyName());
     
     
   }
@@ -139,7 +139,7 @@ public class SpaceLastVisitedTest extends AbstractCoreTest {
     spaces = spaceService.getLastAccessedSpace("mary", null, 0, 5);
     assertEquals(5, spaces.size());
     Space got = spaces.get(0);
-    assertEquals("space_4", got.getPrettyName());
+    assertEquals("space_0", got.getPrettyName());
   }
   
   public void testGet10SpaceLastVisitedAppId() throws Exception {
@@ -155,7 +155,7 @@ public class SpaceLastVisitedTest extends AbstractCoreTest {
     
     assertEquals(5, spaces.size());
     Space got = spaces.get(0);
-    assertEquals("space_9", got.getPrettyName());
+    assertEquals("space_4", got.getPrettyName());
     
     //
     Space space2 = spaceService.getSpaceByPrettyName("space_2");
@@ -164,7 +164,7 @@ public class SpaceLastVisitedTest extends AbstractCoreTest {
     spaces = spaceService.getLastAccessedSpace("mary", "app4", 0, 5);
     assertEquals(5, spaces.size());
     got = spaces.get(0);
-    assertEquals("space_9", got.getPrettyName());
+    assertEquals("space_4", got.getPrettyName());
   }
   
   /**
