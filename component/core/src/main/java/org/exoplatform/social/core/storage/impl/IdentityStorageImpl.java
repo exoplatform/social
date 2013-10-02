17a758b4051230533b85e6f1960ce2e65949597f
binhdv <binhdv@exoplatform.com>
2013-10-02 11:53:48 +0700

    SOC-3763 | [Migration] Space activities are not correctly migrated from Platform 3.5
    Fix description:  in IdentityStorageImpl class, before set activity ID, if ActivityProfileEntity is not null, then new activity will be created.

--- component/core/src/main/java/org/exoplatform/social/core/storage/impl/IdentityStorageImpl.java
+++ component/core/src/main/java/org/exoplatform/social/core/storage/impl/IdentityStorageImpl.java
@@ -1339,8 +1339,11 @@ public class IdentityStorageImpl extends AbstractStorage implements IdentityStor
     try {
       ProfileEntity profileEntity = _findById(ProfileEntity.class, identity.getProfile().getId());
       ActivityProfileEntity activityPEntity = profileEntity.getActivityProfile();
-      type.setActivityId(activityPEntity, activityId);
+      if (activityPEntity == null) {
+        activityPEntity = profileEntity.createActivityProfile();
+      }
       profileEntity.setActivityProfile(activityPEntity);
+      type.setActivityId(activityPEntity, activityId);
       getSession().save();
     } catch (NodeNotFoundException e) {
       LOG.debug(e.getMessage(), e);
