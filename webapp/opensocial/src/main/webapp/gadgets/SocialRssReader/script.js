38bed102c8d200fb8674c4be32048d7c89e190c3
giangnt <giangnt@exoplatform.com>
2013-10-14 18:34:23 +0700

    UI-2995  [PLF 4] Gadget - RSS Reader

--- webapp/opensocial/src/main/webapp/gadgets/SocialRssReader/script.js
+++ webapp/opensocial/src/main/webapp/gadgets/SocialRssReader/script.js
@@ -64,7 +64,7 @@ RssFetch.prototype.loadPage = function() {
   var feedEl = _gel("rssFeed");
   var currentView = gadgets.views.getCurrentView().getName();
 
-  var bullet = "<img src='/social-resources/skin/DefaultSkin/portal/background/favicon.png' tilte='rssIcon' alt='rssIcon' border=0 align='absmiddle' style='height:16;width:16;' onerror='this.style.visibility=\"hidden\";'>&nbsp;&nbsp;";
+  var bullet = "<img src='/social/gadgets/SocialRssReader/images/icon-rss.png' tilte='rssIcon' alt='rssIcon' border=0 align='absmiddle' style='height:16;width:16;' onerror='this.style.visibility=\"hidden\";'>&nbsp;&nbsp;";
 
   while ( feedEl.hasChildNodes() )
   {
