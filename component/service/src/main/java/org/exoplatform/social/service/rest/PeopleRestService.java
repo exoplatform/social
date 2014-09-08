f01f5d23a7c6f828729437fdccb984deab39e170
trungjc <trungnguyenthanh70@gmail.com>
2014-09-08 18:03:12 +0700

    UI-3327: fix wrong patch image

--- component/service/src/main/java/org/exoplatform/social/service/rest/PeopleRestService.java
+++ component/service/src/main/java/org/exoplatform/social/service/rest/PeopleRestService.java
@@ -704,7 +704,7 @@ public class PeopleRestService implements ResourceContainer{
   }
   
   static public class UserInfo {
-    static private String AVATAR_URL = "/eXoSkin/skin/less/social/skin/images/ShareImages/UserAvtDefault.png";
+    static private String AVATAR_URL = "/eXoSkin/skin/images/social/skin/ShareImages/UserAvtDefault.png";
 
     String id;
     String name;
