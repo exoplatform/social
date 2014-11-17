b30bec8352b221966ca5c2b2ffe6aabd11ce6aa0
tu-vu-duy <tuvd@exoplatform.com>
2014-11-17 17:54:46 +0700

    SOC-4502 | User profile: Create UI for UIBasicProfilePortlet

--- webapp/portlet/src/main/java/org/exoplatform/social/user/portlet/UIBasicProfilePortlet.java
+++ webapp/portlet/src/main/java/org/exoplatform/social/user/portlet/UIBasicProfilePortlet.java
@@ -16,6 +16,19 @@
  ***************************************************************************/
 package org.exoplatform.social.user.portlet;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.portlet.PortletMode;
+
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.identity.model.Profile;
+import org.exoplatform.social.webui.Utils;
+import org.exoplatform.webui.application.WebuiApplication;
+import org.exoplatform.webui.application.WebuiRequestContext;
+import org.exoplatform.webui.application.portlet.PortletRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.core.UIPortletApplication;
 import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
@@ -25,8 +38,102 @@ import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
   template = "app:/groovy/social/portlet/user/UIBasicProfilePortlet.gtmpl"
 )
 public class UIBasicProfilePortlet extends UIPortletApplication {
+  private Profile currentProfile;
+  
 
   public UIBasicProfilePortlet() throws Exception {
   }
 
+  public void processRender(WebuiApplication app, WebuiRequestContext context) throws Exception {
+    PortletRequestContext portletReqContext = (PortletRequestContext) context;
+    PortletMode portletMode = portletReqContext.getApplicationMode();
+    if (portletMode == PortletMode.VIEW) {
+      Identity ownerIdentity = Utils.getOwnerIdentity(false);
+      currentProfile = ownerIdentity.getProfile();
+    }
+    //
+    super.processRender(app, context);
+  }
+
+  protected Map<String, Object> getProfileInfo() {
+    Map<String, Object> infos = new LinkedHashMap<String, Object>();
+    infos.put(Profile.EMAIL, currentProfile.getEmail());
+    //
+    String jobTitle = currentProfile.getPosition();
+    if(!isEmpty(jobTitle)) {
+      infos.put(Profile.POSITION, jobTitle);
+    }
+    String gender = currentProfile.getGender();
+    if(!isEmpty(gender)) {
+      infos.put(Profile.GENDER, gender);
+    }
+    //
+    List<Map<String, String>> phones = currentProfile.getPhones();
+    if (phones.size() > 0) {
+      Map<String, String> phoneInfos = new HashMap<String, String>();
+      for (Map<String, String> map : phones) {
+        for (String key : map.keySet()) {
+          phoneInfos.put(key, map.get(key));
+        }
+      }
+      //
+      infos.put(Profile.CONTACT_PHONES, phoneInfos);
+    }
+    //
+    List<Map<String, String>> ims = (List<Map<String, String>>) currentProfile.getProperty(Profile.CONTACT_IMS);
+    if (ims.size() > 0) {
+      Map<String, String> imInfos = new HashMap<String, String>();
+      for (Map<String, String> map : ims) {
+        for (String key : map.keySet()) {
+          imInfos.put(key, map.get(key));
+        }
+      }
+      //
+      infos.put(Profile.CONTACT_IMS, imInfos);
+    }
+    //
+    List<Map<String, String>> urls = (List<Map<String, String>>) currentProfile.getProperty(Profile.CONTACT_URLS);
+    if (urls.size() > 0) {
+      Map<String, String> urlInfos = new HashMap<String, String>();
+      for (Map<String, String> map : urls) {
+        for (String key : map.keySet()) {
+          urlInfos.put(key, map.get(key));
+        }
+      }
+      //
+      infos.put(Profile.CONTACT_URLS, urlInfos);
+    }
+    //
+    return infos;
+  }
+
+  private static boolean isEmpty(String s) {
+    return s == null || s.trim().length() == 0;
+  }
+
+  protected boolean isString(Object s) {
+    return s instanceof String;
+  }
+  
+  
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
