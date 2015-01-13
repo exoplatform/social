6d5daa295bfadafa828e4ea3588fc75d546cdce7
binhdv <binhdv@exoplatform.com>
2015-01-13 17:30:21 +0700

    SOC-4565 | [Intranet Notifications] Accept actions should not trigger a new entry

--- component/service/src/main/java/org/exoplatform/social/service/rest/IntranetNotificationRestService.java
+++ component/service/src/main/java/org/exoplatform/social/service/rest/IntranetNotificationRestService.java
@@ -16,11 +16,8 @@
  */
 package org.exoplatform.social.service.rest;
 
-import java.lang.reflect.Method;
-import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -29,7 +26,6 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.apache.commons.lang.ArrayUtils;
 import org.exoplatform.commons.api.notification.NotificationContext;
 import org.exoplatform.commons.api.notification.NotificationMessageUtils;
 import org.exoplatform.commons.api.notification.channel.AbstractChannel;
@@ -50,23 +46,13 @@ import org.exoplatform.container.PortalContainer;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.services.rest.resource.ResourceContainer;
-import org.exoplatform.social.core.activity.model.ExoSocialActivity;
-import org.exoplatform.social.core.chromattic.entity.ActivityRef;
-import org.exoplatform.social.core.chromattic.entity.ActivityRefListEntity;
-import org.exoplatform.social.core.chromattic.entity.IdentityEntity;
-import org.exoplatform.social.core.chromattic.utils.ActivityRefIterator;
-import org.exoplatform.social.core.chromattic.utils.ActivityRefList;
 import org.exoplatform.social.core.identity.model.Identity;
 import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
-import org.exoplatform.social.core.identity.provider.SpaceIdentityProvider;
-import org.exoplatform.social.core.manager.ActivityManager;
 import org.exoplatform.social.core.manager.IdentityManager;
 import org.exoplatform.social.core.manager.RelationshipManager;
 import org.exoplatform.social.core.space.model.Space;
 import org.exoplatform.social.core.space.spi.SpaceService;
 import org.exoplatform.social.core.storage.impl.AbstractStorage;
-import org.exoplatform.social.core.storage.impl.ActivityStreamStorageImpl.ActivityRefType;
-import org.exoplatform.social.core.storage.impl.StorageUtils;
 
 /**
  * Created by The eXo Platform SAS
@@ -80,73 +66,7 @@ public class IntranetNotificationRestService extends AbstractStorage implements 
   private IdentityManager identityManager;
   private RelationshipManager relationshipManager;
   private SpaceService spaceService;
-  private ActivityManager activityManager;
   
-  private ActivityManager getActivityManager() {
-    if (activityManager == null) {
-      activityManager = (ActivityManager) getPortalContainer().getComponentInstanceOfType(ActivityManager.class);
-    }
-    return activityManager;
-  }
-  
-  @GET
-  @Path("activity/{nbDays}")
-  public void cleanUpActivity(@PathParam("nbDays") int nbDays) throws Exception {
-    Set<String> lastLogins = getLastLogin(nbDays);
-    for (String user : lastLogins) {
-      Identity identity = getIdentityManager().getOrCreateIdentity(OrganizationIdentityProvider.NAME, user, false);
-      IdentityEntity entity = _findById(IdentityEntity.class, identity.getId());
-      
-      //clean space activity ref in the feed stream
-      cleanSapceActivityRef(ActivityRefType.FEED, entity, user);
-      
-      //clean space activity ref in the connection stream
-      cleanSapceActivityRef(ActivityRefType.CONNECTION, entity, user);
-    }
-  }
-  
-  private void cleanSapceActivityRef(ActivityRefType type, IdentityEntity entity, String owner) {
-    ActivityRefListEntity refList = type.refsOf(entity);
-    ActivityRefList list = new ActivityRefList(refList);
-    int size = 0;
-    ActivityRefIterator it = list.iterator();
-    while (it.hasNext()) {
-      ActivityRef current = it.next();
-      size++;
-      ExoSocialActivity a = getActivityManager().getActivity(current.getActivityEntity().getId());
-      if (SpaceIdentityProvider.NAME.equals(a.getActivityStream().getType().toString())) {
-        Space space = getSpaceService().getSpaceByPrettyName(a.getStreamOwner());
-        if (space != null && ! ArrayUtils.contains(space.getMembers(), owner)) {
-          current.getDay().getActivityRefs().remove(current.getName());
-          size--;
-        }
-      }
-    }
-    //re-update size
-    refList.setNumber(size);
-    StorageUtils.persist();
-  }
-
-  private static Set<String> getLastLogin(int aroundDays) {
-    Calendar calendar = Calendar.getInstance();
-    calendar.add(Calendar.DAY_OF_MONTH, 0 - aroundDays);
-    long fromDay = calendar.getTimeInMillis();
-    try {
-      Class<?> cls = Class.forName("org.exoplatform.platform.gadget.services.LoginHistory.LoginHistoryServiceImpl");
-      if (cls != null) {
-        Class<?>[] params = new Class<?>[1];
-        params[0] = Long.TYPE;
-        Method method = cls.getMethod("getLastUsersLogin", params);
-        Object obj = CommonsUtils.getService(cls);
-        return (Set<String>) method.invoke(obj, fromDay);
-      } else {
-        return null;
-      }
-    } catch (Exception e) {
-      return null;
-    }
-  }
-
   /**
    * Processes the "Accept the invitation to connect" action between 2 users and update notification.
    * 
@@ -193,9 +113,10 @@ public class IntranetNotificationRestService extends AbstractStorage implements 
    * @throws Exception
    */
   @GET
-  @Path("ignoreInvitationToConnect/{senderId}/{receiverId}")
+  @Path("ignoreInvitationToConnect/{senderId}/{receiverId}/{notificationId}")
   public void ignoreInvitationToConnect(@PathParam("senderId") String senderId,
-                                             @PathParam("receiverId") String receiverId) throws Exception {
+                                             @PathParam("receiverId") String receiverId,
+                                             @PathParam("notificationId") String notificationId) throws Exception {
     Identity sender = getIdentityManager().getOrCreateIdentity(OrganizationIdentityProvider.NAME, senderId, true);
     Identity receiver = getIdentityManager().getOrCreateIdentity(OrganizationIdentityProvider.NAME, receiverId, true);
     if (sender == null || receiver == null) {
@@ -249,9 +170,10 @@ public class IntranetNotificationRestService extends AbstractStorage implements 
    * @throws Exception
    */
   @GET
-  @Path("ignoreInvitationToJoinSpace/{spaceId}/{userId}")
+  @Path("ignoreInvitationToJoinSpace/{spaceId}/{userId}/{notificationId}")
   public void ignoreInvitationToJoinSpace(@PathParam("spaceId") String spaceId,
-                                              @PathParam("userId") String userId) throws Exception {
+                                              @PathParam("userId") String userId,
+                                              @PathParam("notificationId") String notificationId) throws Exception {
     Space space = getSpaceService().getSpaceById(spaceId);
     if (space == null) {
       throw new WebApplicationException(Response.Status.BAD_REQUEST);
@@ -307,9 +229,10 @@ public class IntranetNotificationRestService extends AbstractStorage implements 
    * @throws Exception
    */
   @GET
-  @Path("refuseRequestToJoinSpace/{spaceId}/{userId}")
+  @Path("refuseRequestToJoinSpace/{spaceId}/{userId}/{notificationId}")
   public void refuseRequestToJoinSpace(@PathParam("spaceId") String spaceId,
-                                           @PathParam("userId") String userId) throws Exception {
+                                           @PathParam("userId") String userId,
+                                           @PathParam("notificationId") String notificationId) throws Exception {
     Space space = getSpaceService().getSpaceById(spaceId);
     if (space == null) {
       throw new WebApplicationException(Response.Status.BAD_REQUEST);
