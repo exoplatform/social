1de801c073a8abafd0220bc9eb3af2ddb0d8d30d
hanhvq <hanhvq@exoplatform.com>
2015-01-15 15:50:12 +0700

    SOC-4503 | [Recent Actvities] Check in case of displaying Viewl All or not.

--- webapp/juzu-portlet/src/main/java/org/exoplatform/social/portlet/recentActivities/RecentActivities.java
+++ webapp/juzu-portlet/src/main/java/org/exoplatform/social/portlet/recentActivities/RecentActivities.java
@@ -36,8 +36,10 @@ import org.exoplatform.social.portlet.userprofile.helpers.UserProfileHelper;
 import org.exoplatform.social.webui.Utils;
 
 public class RecentActivities {
-  private static int NUMBER_ACTIVITIES = 5;
-
+  private static int LATEST_ACTIVITIES_NUM = 5;
+  private static int ACTIVITIES_NUM_TO_CHECK = 6;
+  private boolean hasViewAll = false;
+  
   @Inject
   @Path("index.gtmpl") Template index;
   
@@ -48,10 +50,14 @@ public class RecentActivities {
     
     parameters.put("_ctx", UserProfileHelper.getContext(renderContext));
     parameters.put("activities", getRecentActivities());
-
+    parameters.put("hasViewAll", this.hasViewAll);
     index.render(parameters);
   }
   
+  public void setHasViewAll(boolean hasViewAll) {
+    this.hasViewAll = hasViewAll;
+  }
+
   private List<ExoSocialActivity> getRecentActivities() {
     List<ExoSocialActivity> results = new ArrayList<ExoSocialActivity>();
     RealtimeListAccess<ExoSocialActivity> activitiesListAccess = null;
@@ -62,8 +68,8 @@ public class RecentActivities {
       activitiesListAccess = Utils.getActivityManager().getActivitiesWithListAccess(currentIdentity, Utils.getViewerIdentity());
     }
     
-    results = activitiesListAccess.loadAsList(0, NUMBER_ACTIVITIES);
-    
+    results = activitiesListAccess.loadAsList(0, LATEST_ACTIVITIES_NUM);
+    setHasViewAll(activitiesListAccess.loadAsList(0, ACTIVITIES_NUM_TO_CHECK).size() > LATEST_ACTIVITIES_NUM);
     return results; 
   }
 }
