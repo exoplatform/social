ada7f3f3c72ed934e47946e12ebc93ad4ce7f809
tu-vu-duy <tuvd@exoplatform.com>
2015-02-09 10:53:21 +0700

    SOC-4681| [User Profile] Bugs on user profile page.

--- webapp/portlet/src/main/java/org/exoplatform/social/user/portlet/UIExperienceProfilePortlet.java
+++ webapp/portlet/src/main/java/org/exoplatform/social/user/portlet/UIExperienceProfilePortlet.java
@@ -16,12 +16,16 @@
  */
 package org.exoplatform.social.user.portlet;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.exoplatform.social.core.identity.model.Profile;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
+import org.exoplatform.webui.utils.TimeConvertUtils;
 
 @ComponentConfig(
   lifecycle = UIApplicationLifecycle.class,
@@ -29,6 +33,8 @@ import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
 )
 public class UIExperienceProfilePortlet extends UIAbstractUserPortlet {
   final protected static String EXPERIENCES_IS_CURRENT = Profile.EXPERIENCES_IS_CURRENT;
+  private static final String DATE_FORMAT_MMDDYYYY = "MM/dd/yyyy";
+  private static final String DATE_FORMAT_MMMMDDYYYY = "MMMM dd, yyyy";
 
   public UIExperienceProfilePortlet() throws Exception {
   }
@@ -39,6 +45,32 @@ public class UIExperienceProfilePortlet extends UIAbstractUserPortlet {
   }
   
   protected List<Map<String, String>> getExperience() throws Exception {
-    return UserProfileHelper.getDisplayExperience(currentProfile);
+    List<Map<String, String>> experiences = UserProfileHelper.getDisplayExperience(currentProfile);
+    for (Map<String, String> experience : experiences) {
+      String strartDate = experience.get(Profile.EXPERIENCES_START_DATE);
+      if (!UserProfileHelper.isEmpty(strartDate)) {
+        experience.put(Profile.EXPERIENCES_START_DATE, convertDate(strartDate));
+      }
+      //
+      String endDate = experience.get(Profile.EXPERIENCES_END_DATE);
+      if (!UserProfileHelper.isEmpty(endDate)) {
+        experience.put(Profile.EXPERIENCES_END_DATE, convertDate(endDate));
+      }
+    }
+    return experiences;
+  }
+  
+  /**
+   * Convert string date from format MM/dd/yyyy to MMMM dd, yyy
+   * @param sDate string date input. 
+   * @return
+   */
+  private String convertDate(String sDate) {
+    try {
+      SimpleDateFormat sd = new SimpleDateFormat(DATE_FORMAT_MMDDYYYY, Locale.ENGLISH);
+      return TimeConvertUtils.getFormatDate(sd.parse(sDate), DATE_FORMAT_MMMMDDYYYY);
+    } catch (Exception e) {
+      return sDate;
+    }
   }
 }
