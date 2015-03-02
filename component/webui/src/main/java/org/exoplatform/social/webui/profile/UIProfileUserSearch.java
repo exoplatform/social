1da5a67ea2b8a421bc5bccbb2b6e4949673b52c5
NgocTM <ngoctm@exoplatform.com>
2015-03-02 17:17:48 +0700

    SOC-4718 [People Search] Failed to render the user name containing apostrophe
    Fix description: remove isValidInput in UIProfileUserSearch class.

--- component/webui/src/main/java/org/exoplatform/social/webui/profile/UIProfileUserSearch.java
+++ component/webui/src/main/java/org/exoplatform/social/webui/profile/UIProfileUserSearch.java
@@ -65,16 +65,6 @@ public class UIProfileUserSearch extends UIForm {
    */
   public static final String REG_FOR_SPLIT = "[^_A-Za-z0-9-.\\s[\\n]]";
 
-  /**
-   * PATTERN FOR CHECK RIGHT INPUT VALUE
-   */
-  static final String RIGHT_INPUT_PATTERN = "^[\\p{L}][\\p{L}._\\- \\d]+$";
-
-  /**
-   * ADD PREFIX TO ENSURE ALWAY RIGHT THE PATTERN FOR CHECKING
-   */
-  static final String PREFIX_ADDED_FOR_CHECK = "PrefixAddedForCheck";
-
   /** Empty character. */
   public static final char EMPTY_CHARACTER = '\u0000';
   
@@ -348,14 +338,7 @@ public class UIProfileUserSearch extends UIForm {
               .append((rawSearchMessageStringBuffer.length() > 0 ? " " : "") + uiSearch.getLabel(Profile.EXPERIENCES_SKILLS) + ":" + skills);
           }
           if(rawSearchMessageStringBuffer.length() > 0){
-            uiSearch.setRawSearchConditional(rawSearchMessageStringBuffer.toString());
-            // Eliminates space characters.
-            if(!isValidInput(filter)){
-              filter.setName("@");
-              filter.setCompany("");
-              filter.setSkills("");
-              filter.setPosition("");
-            }
+            uiSearch.setRawSearchConditional(rawSearchMessageStringBuffer.toString());            
           } else {
             uiSearch.setRawSearchConditional(ALL_FILTER);
             filter.setFirstCharacterOfName(EMPTY_CHARACTER);
@@ -393,32 +376,7 @@ public class UIProfileUserSearch extends UIForm {
       if (skills != null && skills.length() > 0) {
         filter.setSkills(Utils.normalizeString(skills));
       }
-    }
-
-
-    /**
-     * Checks input values follow regular expression.
-     *
-     * @param input <code>Object</code>
-     * @return true if the input is properly to regular expression else return false.
-     */
-    private boolean isValidInput(final ProfileFilter input) {
-      // Check contact name
-      String contactName = input.getName();
-      
-      // Eliminate '*' and '%' character in string for checking
-      String contactNameForCheck = null;
-      if (contactName != null) {
-        contactNameForCheck = contactName.replaceAll("[/*%]", "").trim();
-        // Make sure string for checking is started by alphabet character
-        contactNameForCheck = PREFIX_ADDED_FOR_CHECK + contactNameForCheck;
-        if (!contactNameForCheck.matches(RIGHT_INPUT_PATTERN)) {
-          return false;
-        }
-      }
-
-      return true;
-    }
+    }    
   }
 
   /**
