ad62a37cd381b2c42e7cc9b045c89a5a4179101b
AmeniJ <ajalali@exoplatform.com>
2016-05-04 09:40:01 +0100

    [SOC-5292] On user profile, some activities are not translated

--- webapp/portlet/src/main/java/org/exoplatform/social/user/UIRecentActivity.java
+++ webapp/portlet/src/main/java/org/exoplatform/social/user/UIRecentActivity.java
@@ -1,10 +1,14 @@
 package org.exoplatform.social.user;
 
 import org.exoplatform.social.core.activity.model.ExoSocialActivity;
+import org.exoplatform.social.core.processor.I18NActivityProcessor;
 import org.exoplatform.social.user.portlet.UIRecentActivitiesPortlet;
 import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.core.UIContainer;
+
+import java.util.Locale;
+
 @ComponentConfig(
    template = "war:/groovy/social/webui/profile/UIRecentActivity.gtmpl"
 )
@@ -26,6 +30,12 @@ public class UIRecentActivity extends UIContainer {
   }
 
   protected ExoSocialActivity getActivity() {
+    if (activity.getTitleId() != null) {
+      WebuiRequestContext requestContext = WebuiRequestContext.getCurrentInstance();
+      I18NActivityProcessor i18NActivityProcessor = getApplicationComponent(I18NActivityProcessor.class);
+      Locale userLocale = requestContext.getLocale();
+      activity = i18NActivityProcessor.processKeys(activity, userLocale);
+    }
     return this.activity;
   }
 
