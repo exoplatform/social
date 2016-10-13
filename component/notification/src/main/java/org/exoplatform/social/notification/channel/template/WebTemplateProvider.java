03907200e390c3d5ce63382698867998713dcb34
phuong_vu <phuong.vu@exoplatform.com>
2016-10-13 20:50:15 +0700

    [PLF-6818]: Special caracter is not displayed in the site notification

--- component/notification/src/main/java/org/exoplatform/social/notification/channel/template/WebTemplateProvider.java
+++ component/notification/src/main/java/org/exoplatform/social/notification/channel/template/WebTemplateProvider.java
@@ -16,6 +16,8 @@
  */
 package org.exoplatform.social.notification.channel.template;
 
+import org.apache.commons.lang.StringEscapeUtils;
+
 import org.exoplatform.commons.api.notification.NotificationContext;
 import org.exoplatform.commons.api.notification.NotificationMessageUtils;
 import org.exoplatform.commons.api.notification.annotation.TemplateConfig;
@@ -140,8 +142,9 @@ public class WebTemplateProvider extends TemplateProvider {
       return messageInfo.body(body).end();
     }
     
-    private String cutStringByMaxLength(final String st, final int maxLength) {
+    private String cutStringByMaxLength(String st, int maxLength) {
         if (st == null) return st;
+        st = StringEscapeUtils.unescapeHtml(st);
         if (st.length() <= maxLength) return st;
         String noHtmlSt = st.replaceAll("\\<.*?\\>", "");
         if (noHtmlSt.length() <= maxLength) return noHtmlSt;
