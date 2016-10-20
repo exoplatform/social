564b3580362158f50f3840779e240c1f6af80133
Nguyen The Tuyen <tuyennt@exoplatform.com>
2016-10-20 10:18:04 +0700

    SOC-5266: fix the CKEditor display on mobile

--- webapp/resources/src/main/webapp/javascript/eXo/social/webui/UIActivity.js
+++ webapp/resources/src/main/webapp/javascript/eXo/social/webui/UIActivity.js
@@ -307,8 +307,9 @@ var UIActivity = {
     },1200);
   },
   hideLink : function() {
-    $('textarea#composerInput').exoMentions('reset');
-    var container = $('#ComposerContainer')
+    //$('textarea#composerInput').exoMentions('reset');
+    CKEDITOR.instances["composerInput"].setData('');
+    var container = $('#ComposerContainer');
     var link = container.find('#LinkExtensionContainer');
     if (link.length > 0) {
       if (link.css('display') !== 'none') {
@@ -362,14 +363,16 @@ var UIActivity = {
           hidenComposer($(this));
         });
         //
-        composer.find('textarea#composerInput').exoMentions('registerControlButton', function(status) {
+        CKEDITOR.instances["composerInput"].focus();
+
+        /*composer.find('textarea#composerInput').exoMentions('registerControlButton', function(status) {
           var btnSubmit = $('#' + UIActivity.responsiveId).find('.uiComposer:first').find('.btn-submit');
           if(status === true) {
             btnSubmit.removeAttr('disabled');
           } else {
             btnSubmit.prop('disabled', true);
           }
-        });
+        });*/
       });
       //
       var btnGroup = root.find('.button-group');
