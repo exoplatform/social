8ccc5b3a16d2c0702fc47dfd0f910dd0d8338376
Nguyen The Tuyen <tuyennt@exoplatform.com>
2016-10-25 17:07:08 +0700

    PLF-6869 Cancel button does not work properly with smartphone

--- webapp/resources/src/main/webapp/javascript/eXo/social/webui/UIActivity.js
+++ webapp/resources/src/main/webapp/javascript/eXo/social/webui/UIActivity.js
@@ -308,7 +308,15 @@ var UIActivity = {
   },
   hideLink : function() {
     //$('textarea#composerInput').exoMentions('reset');
-    CKEDITOR.instances["composerInput"].setData('');
+    // This is work around, if we call method setData('') of ckeditor,
+    // it will cancel the suggester plugin due to it replace all content in iframe
+    var editor = CKEDITOR.instances["composerInput"];
+    if (editor.mode != 'source') {
+      if (editor.document) $(editor.document.getBody().$).html('');
+    } else {
+      $(editor.container.$).find(".cke_source").html('');
+    }
+
     var container = $('#ComposerContainer');
     var link = container.find('#LinkExtensionContainer');
     if (link.length > 0) {
