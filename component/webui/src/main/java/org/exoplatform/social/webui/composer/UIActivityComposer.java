c2b4c7f0b75278cb0730f3d0e34e0d781cbf67aa
Nguyen The Tuyen <tuyennt@exoplatform.com>
2016-11-11 15:54:31 +0700

    PLF-6919 Warning popup is displayed when selecting File tab for two times

--- component/webui/src/main/java/org/exoplatform/social/webui/composer/UIActivityComposer.java
+++ component/webui/src/main/java/org/exoplatform/social/webui/composer/UIActivityComposer.java
@@ -38,7 +38,6 @@ import org.exoplatform.webui.event.EventListener;
 public abstract class UIActivityComposer extends UIContainer {
   private static final String SELECT_DOCUMENT_ACTION = "SelectDocument";
   private static final String UI_DOCUMENT_ACTIVITY_COMPOSER = "UIDocActivityComposer";
-  private static final String FILE_ALREADY_EXIST = "UIActivityComposer.message.alreadyAttached";
   
   private UIContainer activityDisplay;
   private UIActivityComposerManager activityComposerManager;
@@ -129,11 +128,6 @@ public abstract class UIActivityComposer extends UIContainer {
       final UIActivityComposer activityComposer = event.getSource();
       final UIActivityComposerManager activityComposerManager = activityComposer.getActivityComposerManager();
       activityComposerManager.setCurrentActivityComposer(activityComposer);
-      if ((activityComposer.getClass().getSimpleName().equals(UI_DOCUMENT_ACTIVITY_COMPOSER))&&(activityComposer.isDisplayed())) {
-        UIApplication uiApp = ctx.getUIApplication();
-        uiApp.addMessage(new ApplicationMessage(FILE_ALREADY_EXIST, null, ApplicationMessage.WARNING));
-        return;
-      }
 
       activityComposer.onActivate(event);
       if (!(activityComposer.getClass().getSimpleName().equals(UI_DOCUMENT_ACTIVITY_COMPOSER))) {
