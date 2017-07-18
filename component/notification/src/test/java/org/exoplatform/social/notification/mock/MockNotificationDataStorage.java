5a40dff48fb093fd3614ff6ebe94ba0ea9bce654
wkhessairi <wkhessairi@exoplatform.com>
2017-07-18 16:21:49 +0200

    COMMONS-526: update NotificationDataStorage according to its api

--- component/notification/src/test/java/org/exoplatform/social/notification/mock/MockNotificationDataStorage.java
+++ component/notification/src/test/java/org/exoplatform/social/notification/mock/MockNotificationDataStorage.java
@@ -16,15 +16,15 @@
  */
 package org.exoplatform.social.notification.mock;
 
-import java.util.List;
-import java.util.Map;
-
 import org.exoplatform.commons.api.notification.NotificationContext;
 import org.exoplatform.commons.api.notification.model.NotificationInfo;
 import org.exoplatform.commons.api.notification.model.PluginKey;
 import org.exoplatform.commons.api.notification.model.UserSetting;
 import org.exoplatform.commons.api.notification.service.storage.NotificationDataStorage;
 import org.exoplatform.commons.api.notification.service.storage.NotificationService;
+
+import java.util.List;
+import java.util.Map;
 
 public class MockNotificationDataStorage implements NotificationDataStorage {
   NotificationService notificationService;
@@ -37,7 +37,7 @@ public class MockNotificationDataStorage implements NotificationDataStorage {
   }
 
   @Override
-  public void removeMessageAfterSent() throws Exception {
+  public void removeMessageAfterSent(NotificationContext context) throws Exception {
     
   }
 
