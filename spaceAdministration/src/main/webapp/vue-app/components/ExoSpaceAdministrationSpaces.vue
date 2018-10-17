f0d1ca0da4bbc00fcf96750cfd0ba804a17108fc
seifeddineDaoud <sdaoud@exoplatform.com>
2018-10-17 16:58:41 +0100

    delete folder mocks and file .gitignore

--- spaceAdministration/src/main/webapp/vue-app/components/ExoSpaceAdministrationSpaces.vue
+++ spaceAdministration/src/main/webapp/vue-app/components/ExoSpaceAdministrationSpaces.vue
@@ -1,18 +1,18 @@
 <template>
-  <div id="UITabPane" class="uiTabNormal uiTabInPage">
+  <div class="uiTabNormal uiTabInPage">
     <ul class="nav nav-tabs">
       <li :class="[ activetab === 1 ? 'active' : '' ]" @click="activetab=1">
-        <a data-target="#tab1" data-toggle="tab">Manage Spaces</a>
+        <a data-toggle="tab">Manage Spaces</a>
       </li>
       <li :class="[ activetab === 2 ? 'active' : '' ]" @click="activetab=2" >
-        <a data-target="#tab2" data-toggle="tab">Permissions</a>
+        <a data-toggle="tab">Permissions</a>
       </li>
     </ul> 
     <div class="tab-content">
-      <div v-if="activetab === 1" id="tab1" class="tab-pane fade in active">
+      <div v-if="activetab === 1" class="tab-pane fade in active">
         <exo-space-administration-manage-spaces></exo-space-administration-manage-spaces>
       </div>
-      <div v-if="activetab === 2" id="tab2" class="tab-pane fade in active">
+      <div v-if="activetab === 2" class="tab-pane fade in active">
         <h1>permissssssssssss</h1>
       </div>
     </div> 
@@ -33,49 +33,55 @@ export default {
 };
 </script>
 <style>
-.nav-tabs{
-    background: #ffffff;
+.uiTabInPage .nav-tabs{
+    background: #fff;
+    width: 1024px;
+    border-bottom: 0;
 }
 .nav-tabs > li{
     width: 185px;
-    border-width: 2px;
+    border:none;
 }
 .uiTabInPage{
-    max-width: 782px;
+    border: 1px solid #ccc;
+    max-width: 1026px;
     margin-top: 30px;
     margin-left: auto;
     margin-right: auto;
 }
-.uiTabNormal .nav-tabs>li a {
-    border-color: #e1e8ee;
+.uiTabInPage .nav-tabs>li a {
+    border:none;
     color: #578dc9;
     background-color: #fff;
+    border: none;
+    border-right: none;
 } 
-.uiTabNormal .nav-tabs>li.active a{
-    border-color: #e1e8ee;
-    border-bottom-color: #578dc9;
-    background-color: #fff;
-}
-.uiTabNormal .nav-tabs>.active>a{
-    background-color: #fff;
-    border-color: #e1e8ee;
-    border-bottom-color: #578dc9;
+.uiTabInPage .nav-tabs>li a:hover {
+    border:none;
     color: #578dc9;
-}
-.uiTabNormal .nav-tabs>.active>a{
     background-color: #fff;
-    border-color: #e1e8ee;
-    border-bottom-color: #578dc9;
+    border: none;
+    border-right: none;
+} 
+.uiTabInPage .nav-tabs>li.active a {
+    background-color: #fff;
     color: #578dc9;
+    border: none;
+    border-right: none;
+    border-bottom: 2px solid #578dc9;
 }
-.uiTabNormal .nav-tabs>li.active a {
+.uiTabInPage .nav-tabs>li.active a:hover {
     background-color: #fff;
-    border-color: #e1e8ee;
-    border-bottom-color: #578dc9;
+    border: none;
+    border-right: none;
+    border-bottom: 2px solid #578dc9;
 }
-.uiTabNormal .nav-tabs>li.active a:hover {
-    background-color: #fff;
-    border-color: #e1e8ee;
-    border-bottom-color: #578dc9;
+.uiTabNormal .tab-content {
+  border:none;
+}
+.uiTabNormal .nav-tabs>li:not(.active):last-child a, .uiTabInPage .nav-tabs>li:not(.active):last-child a{
+  border: none;
+  border-right: none;
+  background-color:#fff;
 }
 </style>
---
+++ spaceAdministration/src/main/webapp/vue-app/spaceAdministrationServices.js
@@ -0,0 +1,10 @@
+
+export function getSpaces(){
+    return fetch('/rest/v1/social/spaces/', {credentials: 'include'}).then(resp => resp.json());
+}
+
+export function deleteSpaceById(id){
+  return fetch(`/rest/v1/social/spaces/${id}`, {
+    credentials: 'include', 
+    method: 'delete'});
+}
