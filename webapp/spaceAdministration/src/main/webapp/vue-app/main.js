28c638ad67c43d15ec1e52179c5bd60ceb6e41d9
seifeddineDaoud <sdaoud@exoplatform.com>
2018-10-19 12:35:00 +0100

    change name of portlet

--- webapp/spaceAdministration/src/main/webapp/vue-app/main.js
+++ webapp/spaceAdministration/src/main/webapp/vue-app/main.js
@@ -2,16 +2,18 @@ import Vue from 'vue';
 import app from './components/app.vue';
 
 // getting language of the PLF 
-//const lang = eXo ? eXo.env ? eXo.env.portal ? eXo.env.portal.language ? eXo.env.portal.language : 'en' : 'en' : 'en' : 'en';
+const lang = typeof eXo !== 'undefined' ? eXo.env.portal.language : 'en';
   
-// should expose the locale ressources as REST API
-//const url = `/vue-webpack-sample/mocks/locale_${lang}.json`;
+// should expose the locale ressources as REST API 
+const url = '/spaceAdministration/vue-app/locale_en.json';
 
 // getting locale ressources
-//exoi18n.loadLanguageAsync(lang, url).then(i18n => {
+exoi18n.loadLanguageAsync(lang, url).then(i18n => {
+  console.log(i18n)
 // init Vue app when locale ressources are ready
-new Vue({
-  el: '#space-administration',
-  render: h => h(app)
+  new Vue({
+    el: '#spacesAdministration',
+    render: h => h(app),
+    i18n
+  })
 });
-//})
