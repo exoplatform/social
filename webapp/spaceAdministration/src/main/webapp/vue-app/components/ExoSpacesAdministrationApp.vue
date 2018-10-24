dad9b2344a3fa8abddb85bc892c84c5c6c3abbd6
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2018-10-24 15:54:54 +0200

    Rename Vue components

---
+++ webapp/spaceAdministration/src/main/webapp/vue-app/components/ExoSpacesAdministrationApp.vue
@@ -0,0 +1,13 @@
+<template>
+  <exo-spaces-administration-spaces></exo-spaces-administration-spaces>
+</template>
+
+<script>
+import ExoSpacesAdministrationSpaces from './ExoSpacesAdministrationSpaces.vue';
+
+export default {
+  components: {
+    'exo-spaces-administration-spaces': ExoSpacesAdministrationSpaces
+  }
+};
+</script>
