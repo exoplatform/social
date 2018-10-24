637a1d549a0a02901bce2f3af5293171a5697a50
seifeddineDaoud <sdaoud@exoplatform.com>
2018-10-24 08:57:57 +0100

    change name of portlet

--- webapp/spaceAdministration/webpack.prod.js
+++ webapp/spaceAdministration/webpack.prod.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge');
 const webpackCommonConfig = require('./webpack.common.js');
 
 // the display name of the war
-const app = 'spaceAdministration';
+const app = 'spacesAdministration';
 
 const config = merge(webpackCommonConfig, {
   output: {
