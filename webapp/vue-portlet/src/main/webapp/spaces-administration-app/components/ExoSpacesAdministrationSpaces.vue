5dff5488b7a4e7291d222433e05bed21c18edbb2
seifeddineDaoud <sdaoud@exoplatform.com>
2018-11-19 11:43:30 +0100

    SOC-6143 : add class uiSpaces

--- webapp/vue-portlet/src/main/webapp/spaces-administration-app/components/ExoSpacesAdministrationSpaces.vue
+++ webapp/vue-portlet/src/main/webapp/spaces-administration-app/components/ExoSpacesAdministrationSpaces.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="uiTabNormal uiTabInPage">
-    <ul class="nav nav-tabs">
-      <li :class="{active: activeTab === 1}" @click="activeTab=1">
-        <a data-toggle="tab">Manage Spaces</a>
-      </li>
-      <li :class="{active: activeTab === 2}" @click="activeTab=2" >
-        <a data-toggle="tab">Permissions</a>
-      </li>
-    </ul> 
-    <div class="tab-content">
-      <div v-if="activeTab === 1" class="tab-pane fade in active">
-        <exo-spaces-administration-manage-spaces></exo-spaces-administration-manage-spaces>
-      </div>
-      <div v-if="activeTab === 2" class="tab-pane fade in active">
-        <h1>permissssssssssss</h1>
-      </div>
-    </div> 
+  <div class="uiSpaces">
+    <div class="uiTabNormal uiTabInPage">
+      <ul class="nav nav-tabs">
+        <li :class="{active: activeTab === 1}" @click="activeTab=1">
+          <a data-toggle="tab">Manage Spaces</a>
+        </li>
+        <li :class="{active: activeTab === 2}" @click="activeTab=2" >
+          <a data-toggle="tab">Permissions</a>
+        </li>
+      </ul> 
+      <div class="tab-content">
+        <div v-if="activeTab === 1" class="tab-pane fade in active">
+          <exo-spaces-administration-manage-spaces></exo-spaces-administration-manage-spaces>
+        </div>
+        <div v-if="activeTab === 2" class="tab-pane fade in active">
+          <h1>permissssssssssss</h1>
+        </div>
+      </div> 
+    </div>
   </div>
 </template>
 
