19d3d4a03efa2bd072a03498d0ed4d7c43080159
Fares Maatoug <fmaatoug@exoplatform.com>
2018-12-21 17:11:45 +0100

    SOC-6122: Remove "&" special characters From space Name Restrictions and extend allowed length to 200 characters (#425)
    * SOC-6122: Increase space title length to 50 and add & to the authorised regex
    
    SOC-6122: Remove JCR special characters restrictions and extend allowed length to 200 characters
    
    SOC-6122: Exclude & from not allowed space name characters
    
    * SOC-6122: Test spaceName length on space creation
    
    SOC-6122: Test spaceName length on space creation
    
    SOC-6122: Test spaceName length on space creation
    
    SOC-6122: Test spaceName length on space creation
    
    * SOC-6122: Apply recommended changes
    
    * SOC-6122: Implement Unit Tests
    
    SOC-6122: Implement Unit Tests
    
    * SOC-6122: Centralise pattern and update unit tests
    
    SOC-6122: Centralise pattern and update unit tests
    
    * SOC-6122: Update unit tests
    
    * SOC-6122: fix variable declaration in isValidSpaceName()
    
    SOC-6122: fix variable declaration in isValidSpaceName()
    
    * SOC-6122: fix Build issues
    
    SOC-6122: Build fix
    
    SOC-6122: Build fix
    
    SOC-6122: Build fix

--- component/webui/src/main/java/org/exoplatform/social/webui/space/UISpaceSettings.java
+++ component/webui/src/main/java/org/exoplatform/social/webui/space/UISpaceSettings.java
@@ -47,9 +47,9 @@ public class UISpaceSettings extends UIFormInputSet {
     super(name);
     //
     addUIFormInput(new UIFormStringInput(SPACE_DISPLAY_NAME, SPACE_DISPLAY_NAME, "").
-                   addValidator(MandatoryValidator.class).
-                   addValidator(ExpressionValidator.class, "^([\\p{L}\\s\\d\']+[\\s]?)+$", MSG_INVALID_SPACE_NAME).
-                   addValidator(StringLengthValidator.class, 3, 30));
+            addValidator(MandatoryValidator.class).
+            addValidator(ExpressionValidator.class, "^([\\p{L}\\s\\d\'&]+[\\s]?)+$", MSG_INVALID_SPACE_NAME).
+            addValidator(StringLengthValidator.class, 3, 200));
     addUIFormInput(new UIFormTextAreaInput(SPACE_DESCRIPTION, SPACE_DESCRIPTION, ""));
   }
   
