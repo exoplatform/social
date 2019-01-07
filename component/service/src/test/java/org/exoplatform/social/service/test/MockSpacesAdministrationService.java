42f788af00836ccb2c516a7060ea21dcf5bef32b
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2019-01-07 17:52:22 +0100

    SOC-6143 : add REST endpoints for Spaces Administration settings

--- component/service/src/test/java/org/exoplatform/social/service/test/MockSpacesAdministrationService.java
+++ component/service/src/test/java/org/exoplatform/social/service/test/MockSpacesAdministrationService.java
@@ -3,25 +3,41 @@ package org.exoplatform.social.service.test;
 import org.exoplatform.services.security.MembershipEntry;
 import org.exoplatform.social.core.space.SpacesAdministrationService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MockSpacesAdministrationService implements SpacesAdministrationService {
+
+  private List<MembershipEntry> spacesAdministrators = new ArrayList<>();
+
+  private List<MembershipEntry> spacesCreators = new ArrayList<>();
+
   @Override
   public List<MembershipEntry> getSuperManagersMemberships() {
-    return null;
+    return spacesAdministrators;
   }
 
   @Override
   public void updateSuperManagersMemberships(List<MembershipEntry> permissionsExpressions) {
+    if(permissionsExpressions != null) {
+      spacesAdministrators = new ArrayList<>(permissionsExpressions);
+    } else {
+      spacesAdministrators.clear();
+    }
   }
 
   @Override
   public List<MembershipEntry> getSuperCreatorsMemberships() {
-    return null;
+    return spacesCreators;
   }
 
   @Override
   public void updateSpacesCreatorsMemberships(List<MembershipEntry> permissionsExpressions) {
+    if(permissionsExpressions != null) {
+      spacesCreators = new ArrayList<>(permissionsExpressions);
+    } else {
+      spacesCreators.clear();
+    }
   }
 
   @Override
