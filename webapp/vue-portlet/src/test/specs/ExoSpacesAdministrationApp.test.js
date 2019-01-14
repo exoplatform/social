aaf3b552ab123d38f9a35fb65b723c7cccf9f2d8
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2019-01-14 23:37:52 +0100

    SOC-6143 : add unit tests on spaces search

--- webapp/vue-portlet/src/test/specs/ExoSpacesAdministrationApp.test.js
+++ webapp/vue-portlet/src/test/specs/ExoSpacesAdministrationApp.test.js
@@ -19,7 +19,7 @@ describe('ExoSpacesAdministrationManageSpaces.test.js', () => {
       }
     ],
     showConfirmMessageModal: false
-  }
+  };
   beforeEach(() => {
     cmp = shallow(ExoSpacesAdministrationManageSpaces, {
       stubs: {
@@ -33,21 +33,51 @@ describe('ExoSpacesAdministrationManageSpaces.test.js', () => {
         
   });
 
-  it('2 displayed spaces', () => {
+  it('should display 2 spaces in table when 2 spaces in data', () => {
     cmp.vm.spaces = data.spaces;
-    expect(cmp.vm.spaces).toHaveLength(2);
+    cmp.vm.$nextTick(() => {
+      const spacesTableRows = cmp.findAll('.manageSpaces table tr');
+      expect(spacesTableRows).toHaveLength(3); // header + 2 rows
+    });
   });
   
-  it('show confirm message modal', () => {
+  it('should show confirm message modal when deleting space', () => {
     cmp.vm.deleteSpaceById(1, 0);
     expect(cmp.vm.showConfirmMessageModal).toBeTruthy();
   });
   
-  it('close confirm message when click on close icon', () => {
+  it('should close confirm message modal when clicking on close icon', () => {
     cmp.vm.showConfirmMessageModal = true;
     const closeButton = cmp.find('.btn');
     expect(cmp.vm.showConfirmMessageModal).toBe(true);
     closeButton.trigger('click');
     expect(cmp.vm.showConfirmMessageModal).toBe(false);
+  });
+
+  it('should fetch spaces with keyword when a search is triggered', () => {
+    const mockJsonPromise = Promise.resolve({'spaces': [
+      {
+        'id': '1',
+        'displayName': 'space1',
+        'description': 'space1 is the first space'
+      }]
+    });
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise,
+    });
+    global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+
+    const showInputSearch = cmp.find('.showInputSearch');
+    const showIconSearch = cmp.find('.uiIconPLF24x24Search');
+    
+    showInputSearch.element.value = 'space1';
+    showInputSearch.trigger('input');
+
+    showIconSearch.trigger('click');
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith('portal/rest/v1/social/spaces?q=space1&limit=30&returnSize=true', {'credentials': 'include'});
+
+    global.fetch.mockClear();
   });
 });
