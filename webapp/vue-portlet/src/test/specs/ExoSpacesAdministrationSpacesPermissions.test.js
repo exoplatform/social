1ba7aa4a031c8e23c8621d2d39a58bc062050a91
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2019-01-15 12:00:33 +0100

    SOC-6143 : add tests on ExoSpacesAdministrationSpacesPermissions component

---
+++ webapp/vue-portlet/src/test/specs/ExoSpacesAdministrationSpacesPermissions.test.js
@@ -0,0 +1,46 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { spaceConstants } from '../../main/webapp/spaces-administration-app/spaceAdministrationConstants.js';
+import ExoSpacesAdministrationSpacesPermissions from '../../main/webapp/spaces-administration-app/components/ExoSpacesAdministrationSpacesPermissions';
+
+const localVue = createLocalVue();
+
+// mock exo-tooltip directive
+localVue.directive('exo-tooltip', function() {});
+
+describe('ExoSpacesAdministrationSpacesPermissions.test.js', () => {
+  let cmp;
+  const data = {
+    creators: ['*:/group1', '*:/group2'],
+    administrators: ['*:/group3'],
+  };
+  beforeEach(() => {
+    cmp = shallowMount(ExoSpacesAdministrationSpacesPermissions, {
+      localVue,
+      stubs: {
+      },
+      mocks: {
+        $t: () => {},
+        $constants : spaceConstants
+      }
+    });
+        
+  });
+
+  it('should display 2 permissions rows with the right values', () => {
+    cmp.vm.creators = data.creators;
+    cmp.vm.administrators = data.administrators;
+
+    const permissionsTableRow = cmp.findAll('.spacesPermissions table tr');
+    expect(permissionsTableRow).toHaveLength(3); // header + 2 rows
+
+    const creatorsPermissions = permissionsTableRow.at(1).findAll('td').at(1);
+    expect(creatorsPermissions.html().indexOf('*:/group1') >= 0).toBe(true);
+    expect(creatorsPermissions.html().indexOf('*:/group2') >= 0).toBe(true);
+    expect(creatorsPermissions.html().indexOf('*:/group3') >= 0).toBe(false);
+
+    const manageSpacesPermissions = permissionsTableRow.at(2).findAll('td').at(1);
+    expect(manageSpacesPermissions.html().indexOf('*:/group1') >= 0).toBe(false);
+    expect(manageSpacesPermissions.html().indexOf('*:/group2') >= 0).toBe(false);
+    expect(manageSpacesPermissions.html().indexOf('*:/group3') >= 0).toBe(true);
+  });
+});
