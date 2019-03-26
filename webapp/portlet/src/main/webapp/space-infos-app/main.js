fcf706218fc58b90e67efffb8a697b3211a9de0f
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2019-03-26 13:01:27 +0100

    SWF-4697 : do not use extensionRegistry module since it does not exists yet in 5.1

--- webapp/portlet/src/main/webapp/space-infos-app/main.js
+++ webapp/portlet/src/main/webapp/space-infos-app/main.js
@@ -7,16 +7,6 @@ const lang = typeof eXo !== 'undefined' ? eXo.env.portal.language : 'en';
 // should expose the locale ressources as REST API 
 const url = `${spacesConstants.PORTAL}/${spacesConstants.PORTAL_REST}/i18n/bundle/locale.portlet.social.SpaceInfosPortlet-${lang}.json`;
 
-// get overrided components if exists
-if (extensionRegistry) {
-  const components = extensionRegistry.loadComponents('SpaceInfos');
-  if (components && components.length > 0) {
-    components.forEach(cmp => {
-      Vue.component(cmp.componentName, cmp.componentOptions);
-    });
-  }
-}
-
 // getting locale ressources
 export function init() {
   exoi18n.loadLanguageAsync(lang, url).then(i18n => {
