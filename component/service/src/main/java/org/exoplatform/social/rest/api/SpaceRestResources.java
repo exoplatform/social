44374fc288b56d1b9982d4f20610952fe97841d6
wkhessairi <wkhessairi@exoplatform.com>
2019-04-12 12:37:48 +0100

    Update EntityBuilder to manage Etag for retrieving space managers correctly

--- component/service/src/main/java/org/exoplatform/social/rest/api/SpaceRestResources.java
+++ component/service/src/main/java/org/exoplatform/social/rest/api/SpaceRestResources.java
@@ -25,6 +25,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
