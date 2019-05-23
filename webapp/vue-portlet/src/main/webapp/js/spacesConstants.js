7aa06de81cac155147fec8a1de7e535929d30932
wkhessairi <wkhessairi@exoplatform.com>
2019-05-23 15:27:31 +0100

    Add missing js file

---
+++ webapp/vue-portlet/src/main/webapp/js/spacesConstants.js
@@ -0,0 +1,15 @@
+export const spacesConstants = {
+  ENV: eXo.env.portal || '',
+  PORTAL: eXo.env.portal.context || '',
+  PORTAL_NAME: eXo.env.portal.portalName || '',
+  PORTAL_REST: eXo.env.portal.rest,
+  PROFILE_SPACE_LINK: '/g/:spaces:',
+  SOCIAL_USER_API: `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/users/`,
+  SOCIAL_SPACE_API: `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/spaces`,
+  SPACES_ADMINISTRATION_API: `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/spacesAdministration`,
+  USER_API: `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/users`,
+  GROUP_API: `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/groups`,
+  SPACES_PER_PAGE: 30,
+  DEFAULT_SPACE_AVATAR: '/eXoSkin/skin/images/system/SpaceAvtDefault.png',
+  SPACES_TEMPLATES_API: `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/spaceTemplates`,
+};
