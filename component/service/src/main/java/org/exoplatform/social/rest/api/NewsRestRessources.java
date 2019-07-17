988d5d5ed7059ba90f6d40a39d34d0972ebce564
mohamedHedi1990 <42541332+mohamedHedi1990@users.noreply.github.com>
2019-07-17 15:30:04 +0100

    Change style for full news activity stream

--- component/service/src/main/java/org/exoplatform/social/rest/api/NewsRestRessources.java
+++ component/service/src/main/java/org/exoplatform/social/rest/api/NewsRestRessources.java
@@ -1,15 +1,10 @@
 package org.exoplatform.social.rest.api;
 
 import java.util.Map;
-
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 public interface NewsRestRessources extends SocialRest {
 
-  public Response clickOnNews(@Context UriInfo uriInfo, @PathParam("id") String id, Map<String, String> tragetField);
+  public Response clickOnNews(UriInfo uriInfo, String id, Map<String, String> targetField);
 }
