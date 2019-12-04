300cc6e3c5f8748d7750719ac56c73a457d36727
Boubaker Khanfir <bkhanfir@exoplatform.com>
2019-12-04 18:34:31 +0100

    Add Legacy Space REST Service moved from Platform project

---
+++ component/service/src/main/java/org/exoplatform/social/rest/api/LegacySpaceRestServiceImpl.java
@@ -0,0 +1,154 @@
+/**
+ * Copyright ( C ) 2012 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.exoplatform.social.rest.api;
+
+import java.util.*;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import org.exoplatform.commons.utils.ListAccess;
+import org.exoplatform.container.PortalContainer;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.services.rest.resource.ResourceContainer;
+import org.exoplatform.social.core.space.SpaceFilter;
+import org.exoplatform.social.core.space.model.Space;
+import org.exoplatform.social.core.space.spi.SpaceService;
+import org.exoplatform.social.rest.impl.space.SpaceRestResourcesV1;
+
+/**
+ * @author <a href="kmenzli@exoplatform.com">kmenzli</a>
+ * @deprecated use {@link SpaceRestResourcesV1} instead
+ */
+@Path("/space")
+@Deprecated
+public class LegacySpaceRestServiceImpl implements ResourceContainer {
+
+  private static final Log   LOG                          = ExoLogger.getLogger(LegacySpaceRestServiceImpl.class);
+
+  private final SpaceService spaceService;
+
+  private final CacheControl cacheControl;
+
+  private static final int   MAX_LOADED_SPACES_BY_REQUEST = 20;
+
+  public LegacySpaceRestServiceImpl(SpaceService spaceService) {
+    this.spaceService = spaceService;
+    cacheControl = new CacheControl();
+    cacheControl.setNoCache(true);
+    cacheControl.setNoStore(true);
+  }
+
+  @GET
+  @Path("/user/searchSpace/")
+  public Response searchSpaces(@QueryParam("keyword") String keyword,
+                               @QueryParam("fields") String fields,
+                               @Context SecurityContext sc) {
+    List<Space> spaces = new ArrayList<Space>();
+    try {
+
+      List<Space> alphabeticallySort = new ArrayList<Space>();
+      String userId = sc.getUserPrincipal().getName();
+      if (userId == null) {
+        return Response.status(500).cacheControl(cacheControl).build();
+      }
+      ListAccess<Space> listAccess = null;
+      if ((keyword == null) || (keyword.equals(""))) {
+        listAccess = spaceService.getMemberSpacesByFilter(userId, null);
+      } else {
+        listAccess = spaceService.getMemberSpacesByFilter(userId, new SpaceFilter(keyword));
+      }
+      // --- List of searchedSpaces
+      List<Space> spacesSearched = new ArrayList(Arrays.asList(listAccess.load(0, MAX_LOADED_SPACES_BY_REQUEST)));
+      // --- List of spaces sorted by access
+      ListAccess<Space> allSpacesSorted = spaceService.getLastAccessedSpace(userId, null);
+      // --- Convert user spaces to List collection
+      spaces = Arrays.asList(allSpacesSorted.load(0, MAX_LOADED_SPACES_BY_REQUEST));
+
+      List<Object> sortedSearchedSpaces = new ArrayList<Object>();
+
+      for (Space space : spaces) {
+        Space filteredSpace = filterSpace(space.getId(), spacesSearched);
+        if (filteredSpace != null) {
+          spacesSearched.remove(filteredSpace);
+          addSpaceWithFieldsToList(space, fields, sortedSearchedSpaces);
+        }
+      }
+      for (Space space : spacesSearched) {
+        addSpaceWithFieldsToList(space, fields, sortedSearchedSpaces);
+      }
+      return Response.ok(sortedSearchedSpaces, "application/json").cacheControl(cacheControl).build();
+    } catch (Exception ex) {
+      if (LOG.isWarnEnabled()) {
+        LOG.warn("An exception happens when searchSpaces", ex);
+      }
+    }
+    return Response.status(500).cacheControl(cacheControl).build();
+  }
+
+  private void addSpaceWithFieldsToList(Space space, String fields, List<Object> sortedSearchedSpaces) {
+    String groupId = space.getGroupId();
+    String permanentSpaceName = groupId.split("/")[2];
+    StringBuffer baseSpaceURL = new StringBuffer();
+    baseSpaceURL.append(PortalContainer.getCurrentPortalContainerName() + "/g/:spaces:");
+    baseSpaceURL.append(permanentSpaceName);
+    baseSpaceURL.append("/");
+    baseSpaceURL.append(space.getPrettyName());
+    space.setUrl(baseSpaceURL.toString());
+    sortedSearchedSpaces.add(extractObject(space, fields));
+  }
+
+  private static Space filterSpace(String spaceId, List<Space> spacesSearched) {
+    for (Space space : spacesSearched) {
+      if (space.getId().equalsIgnoreCase(spaceId)) {
+        return space;
+      }
+    }
+    return null;
+  }
+
+  private Object extractObject(Object from, String fields) {
+    if (fields != null) {
+      String[] f = fields.split(",");
+
+      if (f.length > 0) {
+        JSONObject obj = new JSONObject(from);
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        for (String name : f) {
+          if (obj.has(name)) {
+            try {
+              map.put(name, obj.get(name));
+            } catch (JSONException e) {
+              if (LOG.isWarnEnabled()) {
+                LOG.warn("The key does NOT exist", e);
+              }
+            }
+          }
+        }
+        return map;
+      }
+    }
+    return from;
+  }
+}
---
+++ component/service/src/test/java/org/exoplatform/social/service/rest/BaseRestServicesTestCase.java
@@ -0,0 +1,136 @@
+package org.exoplatform.social.service.rest;
+
+import java.lang.reflect.*;
+import java.security.Principal;
+import java.util.Map;
+
+import javax.ws.rs.core.SecurityContext;
+
+import org.exoplatform.commons.utils.ListAccess;
+import org.exoplatform.component.test.*;
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.services.rest.impl.*;
+import org.exoplatform.services.rest.tools.ResourceLauncher;
+import org.exoplatform.services.test.mock.MockPrincipal;
+
+@ConfiguredBy({
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/portal/configuration.xml"),
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.test.jcr-configuration.xml"),
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.social.test.portal-configuration.xml"),
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.identity-configuration.xml"),
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.portal-configuration.xml"),
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.social.test.jcr-configuration.xml"),
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.social.component.common.test.configuration.xml"),
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.social.component.core.test.configuration.xml"),
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.social.component.core.test.application.registry.configuration.xml"),
+  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/standalone/exo.social.component.service.test.configuration.xml")
+})
+public abstract class BaseRestServicesTestCase extends AbstractKernelTest {
+
+  protected ProviderBinder   providers;
+
+  protected ResourceBinder   binder;
+
+  protected ResourceLauncher launcher;
+
+  protected ExoContainer     container;
+
+  public void setUp() throws Exception {
+    begin();
+    ExoContainer container = getContainer();
+    this.container = container;
+
+    binder = (ResourceBinder) container.getComponentInstanceOfType(ResourceBinder.class);
+    RequestHandlerImpl requestHandler = (RequestHandlerImpl) container.getComponentInstanceOfType(RequestHandlerImpl.class);
+
+    // reset default providers to be sure it is clean.
+    ProviderBinder.setInstance(new ProviderBinder());
+    providers = ProviderBinder.getInstance();
+
+    binder.clear();
+
+    ApplicationContextImpl.setCurrent(new ApplicationContextImpl(null, null, providers, null));
+
+    launcher = new ResourceLauncher(requestHandler);
+    registry(getComponentClass());
+  }
+
+  public void tearDown() throws Exception {
+    unregistry(getComponentClass());
+    end();
+  }
+
+  protected abstract Class<?> getComponentClass();
+
+  protected <T> T getService(Class<T> clazz) {
+    return clazz.cast(this.container.getComponentInstanceOfType(clazz));
+  }
+
+  private void registry(Class<?> resourceClass) throws Exception {
+    binder.addResource(resourceClass, null);
+  }
+
+  private void unregistry(Class<?> resourceClass) {
+    binder.removeResource(resourceClass);
+  }
+
+  protected <T> T createProxy(Class<T> type, final Map<String, Object> result) {
+    Object o = Proxy.newProxyInstance(getClass().getClassLoader(),
+                                      new Class<?>[] { type },
+                                      new InvocationHandler() {
+                                        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                                          if (method.getName().equals("equals")) {
+                                            return proxy == args[0];
+                                          }
+                                          Object o = result.get(method.getName());
+                                          return o instanceof Invoker ? ((Invoker) o).invoke(args) : o;
+                                        }
+                                      });
+    return type.cast(o);
+  }
+
+  protected static interface Invoker {
+    Object invoke(Object[] args);
+  }
+
+  protected static class MockSecurityContext implements SecurityContext {
+
+    private final String username;
+
+    public MockSecurityContext(String username) {
+      this.username = username;
+    }
+
+    public Principal getUserPrincipal() {
+      return new MockPrincipal(username);
+    }
+
+    public boolean isUserInRole(String role) {
+      return false;
+    }
+
+    public boolean isSecure() {
+      return false;
+    }
+
+    public String getAuthenticationScheme() {
+      return null;
+    }
+  }
+
+  protected static class MockListAccess<T> implements ListAccess<T> {
+    private final T[] values;
+
+    public MockListAccess(T[] values) {
+      this.values = values;
+    }
+
+    public T[] load(int index, int length) throws Exception, IllegalArgumentException {
+      return values;
+    }
+
+    public int getSize() throws Exception {
+      return values.length;
+    }
+  }
+}
