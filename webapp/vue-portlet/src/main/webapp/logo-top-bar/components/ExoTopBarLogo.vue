6c5a3a7bec2dc9ad55ddf8798c16a3cec9ee7465
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-03-16 13:06:29 +0100

    PLF-8737 Move application registry definition to layout-management addon (#696)

--- webapp/vue-portlet/src/main/webapp/logo-top-bar/components/ExoTopBarLogo.vue
+++ webapp/vue-portlet/src/main/webapp/logo-top-bar/components/ExoTopBarLogo.vue
@@ -1,14 +1,14 @@
 <template>
-  <v-app id="brandingToBar" class="border-box-sizing" flat>
+  <v-app id="brandingTopBar" class="border-box-sizing" flat>
     <v-container pa-3>
-      <v-row class="mx-0">
+      <div class="d-flex mx-0 pa-0">
         <a :href="homeLink" class=" pr-3 logoContainer">
           <v-img src="/portal/rest/v1/platform/branding/logo" max-width="75" max-height="50"/>
         </a>
         <a v-show="brandingCompanyName" :href="homeLink" class=" pl-2 align-self-center brandingContainer">
           <span class="subtitle-2 font-weight-bold">{{ brandingCompanyName }}</span>
         </a>
-      </v-row>
+      </div>
     </v-container>
   </v-app>
 </template>
