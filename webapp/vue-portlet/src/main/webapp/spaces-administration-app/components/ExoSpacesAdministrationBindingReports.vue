d4694271b8c6739344262f7b3bcc57c2dbd02c2d
Moez Diha <38471028+mdiha@users.noreply.github.com>
2020-03-19 13:21:08 +0100

    SOC-6150: Binding_reports_US02: Add binding reports table. (#710)

--- webapp/vue-portlet/src/main/webapp/spaces-administration-app/components/ExoSpacesAdministrationBindingReports.vue
+++ webapp/vue-portlet/src/main/webapp/spaces-administration-app/components/ExoSpacesAdministrationBindingReports.vue
@@ -1,13 +1,64 @@
 <template>
-  <div>
-    <v-skeleton-loader
-      class="mx-auto"
-      type="table-heading,table-tbody">
-    </v-skeleton-loader>
+  <div id="bindingReports">
+    <div v-show="loading">
+      <v-skeleton-loader
+        class="mx-auto"
+        type="table-heading,table-tbody">
+      </v-skeleton-loader>
+    </div>
+    <div v-show="!loading">
+      <v-card-title>
+        <v-layout justify-space-between>
+          <v-flex xs7></v-flex>
+          <v-flex xs2>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              single-line
+              solo
+              flat
+              hide-details>
+            </v-text-field>
+          </v-flex>
+          <v-flex xs2>
+            <v-select
+              :items="items"
+              label="All bindings"
+              solo
+              flat
+            ></v-select>
+          </v-flex>          
+        </v-layout>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :search="search"
+        disable-sort
+      ></v-data-table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      loading: false,
+      search: '',
+      items: ['All bindings', 'New binding', 'Removing binding', 'Synchronization'],
+      desserts: [],
+      headers: [
+        { text: 'Space',value: 'space' },
+        { text: 'Group', value: 'group' },
+        { text: 'Start date', value: 'startDate' },
+        { text: 'End date', value: 'endDate' },
+        { text: 'Operation Type', value: 'operationType' },
+        { text: 'Added users', value: 'addedUsers' },
+        { text: 'Removed users', value: 'removedUsers' },
+        { text: 'File', value: 'file' },
+      ],
+    };
+  }
 };
 </script>
